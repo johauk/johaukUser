61e942cb5193764b5f0b6c3ccedaf35aade337e8
hauk <>
2010-04-12 14:46:10 +0000

    Function added to handle standard 2D correlation histos easier

--- ApeAndCpeStudies/FullVersion/ApeEstimator/ApeEstimator/interface/TrackerSectorStruct.h
+++ ApeAndCpeStudies/FullVersion/ApeEstimator/ApeEstimator/interface/TrackerSectorStruct.h
@@ -3,84 +3,80 @@
 
 
 //#include "vector"
-#include "TH1.h"
-#include "TH2.h"
-//#include "TProfile.h"
+#include "TH1F.h"
+#include "TH2F.h"
+#include "TProfile.h"
+#include "TString.h"
+//#include "TFileDirectory.h"
 
-struct TrackerSectorStruct{
+#include "EventVariables.h"
+
+class TH1;
+class TH2;
+//class TH1F;
+//class TH2F;
+//class TProfile;
+//class TString;
+class TFileDirectory;
+
+
+
+
+
+
+
+
+
+class TrackerSectorStruct{
+  public:
   
-  TrackerSectorStruct(): IsModuleUsable(0),
-                         Width(0), MaxStrip(0), Charge(0), MaxCharge(0), MaxIndex(0), ChargeLR(0),
-			 BaryStrip(0), SOverN(0),
+  TrackerSectorStruct(): directory_(0),
+                         norResXMax_(999.), sigmaXHitMax_(999.), sigmaXMax_(999.),
+			 apeX(-1.F),
 			 ResX(0), NorResX(0), XHit(0), XTrk(0),
-			 SigmaXHit(0), SigmaXTrk(0), SigmaX(0), SigmaX2(0),
-			 PhiSens(0), PhiSensX(0), PhiSensY(0),
-			 NorResXVsWidth(0), NorResXVsMaxStrip(0), NorResXVsCharge(0), NorResXVsMaxCharge(0), NorResXVsMaxIndex(0), NorResXVsChargeLR(0),
-                         NorResXVsBaryStrip(0), NorResXVsSOverN(0),
-			 NorResXVsP(0), NorResXVsMeanAngle(0), NorResXVsHitsValid(0), NorResXVsHitsGood(0),
-			 NorResXVsHitsInvalid(0), NorResXVsLayersMissed(0), NorResXVsNorChi2(0),
-			 NorResXVsPhiSens(0), NorResXVsPhiSensX(0), NorResXVsPhiSensY(0),
-			 SigmaXHitVsWidth(0), SigmaXHitVsMaxStrip(0), SigmaXHitVsCharge(0), SigmaXHitVsMaxCharge(0), SigmaXHitVsMaxIndex(0), SigmaXHitVsChargeLR(0),
-                         SigmaXHitVsBaryStrip(0), SigmaXHitVsSOverN(0),
-			 SigmaXTrkVsP(0), SigmaXTrkVsInvP(0), SigmaXTrkVsMeanAngle(0), SigmaXTrkVsHitsValid(0),
-			 SigmaXTrkVsHitsGood(0), SigmaXTrkVsHitsInvalid(0), SigmaXTrkVsLayersMissed(0), SigmaXVsNorChi2(0),
-			 SigmaXHitVsPhiSens(0), SigmaXHitVsPhiSensX(0), SigmaXHitVsPhiSensY(0),
-			 SigmaXTrkVsPhiSens(0), SigmaXTrkVsPhiSensX(0), SigmaXTrkVsPhiSensY(0),
-			 SigmaXVsPhiSens(0), SigmaXVsPhiSensX(0), SigmaXVsPhiSensY(0),
-			 NorResXVsSigmaXHit(0), NorResXVsSigmaXTrk(0), NorResXVsSigmaX(0),
-			 WidthVsPhiSensX(0),
-			 PNorResXVsWidth(0), PNorResXVsMaxStrip(0), PNorResXVsCharge(0), PNorResXVsMaxCharge(0), PNorResXVsMaxIndex(0), PNorResXVsChargeLR(0),
-                         PNorResXVsBaryStrip(0), PNorResXVsSOverN(0),
-			 PNorResXVsP(0), PNorResXVsMeanAngle(0), PNorResXVsHitsValid(0), PNorResXVsHitsGood(0),
-			 PNorResXVsHitsInvalid(0), PNorResXVsLayersMissed(0), PNorResXVsNorChi2(0),
-			 PNorResXVsPhiSens(0), PNorResXVsPhiSensX(0), PNorResXVsPhiSensY(0),
-			 PSigmaXHitVsWidth(0), PSigmaXHitVsMaxStrip(0), PSigmaXHitVsCharge(0), PSigmaXHitVsMaxCharge(0), PSigmaXHitVsMaxIndex(0), PSigmaXHitVsChargeLR(0),
-                         PSigmaXHitVsBaryStrip(0), PSigmaXHitVsSOverN(0),
-			 PSigmaXTrkVsP(0), PSigmaXTrkVsInvP(0), PSigmaXTrkVsMeanAngle(0), PSigmaXTrkVsHitsValid(0),
-			 PSigmaXTrkVsHitsGood(0), PSigmaXTrkVsHitsInvalid(0), PSigmaXTrkVsLayersMissed(0), PSigmaXVsNorChi2(0),
-			 PSigmaXHitVsPhiSens(0), PSigmaXHitVsPhiSensX(0), PSigmaXHitVsPhiSensY(0),
-			 PSigmaXTrkVsPhiSens(0), PSigmaXTrkVsPhiSensX(0), PSigmaXTrkVsPhiSensY(0),
-			 PSigmaXVsPhiSens(0), PSigmaXVsPhiSensX(0), PSigmaXVsPhiSensY(0),
-			 PNorResXVsSigmaXHit(0), PNorResXVsSigmaXTrk(0), PNorResXVsSigmaX(0),
-			 PWidthVsPhiSensX(0){}
+			 SigmaX2(0), ProbX(0),
+			 WidthVsPhiSensX(0), WidthVsWidthProjected(0), WidthDiffVsMaxStrip(0),
+			 PWidthVsPhiSensX(0), PWidthVsWidthProjected(0), PWidthDiffVsMaxStrip(0){}
+  
+  
+  
+  
+  struct CorrelationHists{
+    CorrelationHists():Variable(0),
+                       NorResXVsVar(0), ProbXVsVar(0),
+	               SigmaXHitVsVar(0), SigmaXTrkVsVar(0), SigmaXVsVar(0),
+		       PNorResXVsVar(0), PProbXVsVar(0),
+		       PSigmaXHitVsVar(0), PSigmaXTrkVsVar(0), PSigmaXVsVar(0){}
+    
+    void fillCorrHists(const TrackStruct::HitParameterStruct& hitParameterStruct, double variable);
+    
+    TH1F *Variable;
+    TH2F *NorResXVsVar, *ProbXVsVar,
+         *SigmaXHitVsVar, *SigmaXTrkVsVar, *SigmaXVsVar;
+    TProfile *PNorResXVsVar, *PProbXVsVar,
+             *PSigmaXHitVsVar, *PSigmaXTrkVsVar, *PSigmaXVsVar;
+  };
+  
+  
+  void setCorrHistParams(TFileDirectory*, double, double, double);
+  CorrelationHists bookCorrHists(TString, TString, TString, TString, int, int, double, double, std::string ="nphtr");
+  /// same, but without booking 1D histo 
+  CorrelationHists bookCorrHists(TString ,TString ,TString, int, double, double, std::string ="nphtr");
+  //void fillCorrHists(double, const TrackStruct::HitParameterStruct&, CorrelationHists&);
+  
+  TFileDirectory *directory_;
+  double norResXMax_, sigmaXHitMax_, sigmaXMax_;
+  std::map<std::string,CorrelationHists> mCorrelationHists;
   
   
   std::vector<unsigned int> vRawId;
   
-  TH1 *IsModuleUsable,
-      *Width, *MaxStrip, *Charge, *MaxCharge, *MaxIndex, *ChargeLR,
-      *BaryStrip, *SOverN,
-      *ResX, *NorResX, *XHit, *XTrk,
-      *SigmaXHit, *SigmaXTrk, *SigmaX, *SigmaX2,
-      *PhiSens, *PhiSensX, *PhiSensY;
-  TH2 *NorResXVsWidth, *NorResXVsMaxStrip, *NorResXVsCharge, *NorResXVsMaxCharge, *NorResXVsMaxIndex, *NorResXVsChargeLR,
-      *NorResXVsBaryStrip, *NorResXVsSOverN,
-      *NorResXVsP, *NorResXVsMeanAngle, *NorResXVsHitsValid, *NorResXVsHitsGood,
-      *NorResXVsHitsInvalid, *NorResXVsLayersMissed, *NorResXVsNorChi2,
-      *NorResXVsPhiSens, *NorResXVsPhiSensX, *NorResXVsPhiSensY,
-      *SigmaXHitVsWidth, *SigmaXHitVsMaxStrip, *SigmaXHitVsCharge, *SigmaXHitVsMaxCharge, *SigmaXHitVsMaxIndex, *SigmaXHitVsChargeLR,
-      *SigmaXHitVsBaryStrip, *SigmaXHitVsSOverN,
-      *SigmaXTrkVsP, *SigmaXTrkVsInvP, *SigmaXTrkVsMeanAngle, *SigmaXTrkVsHitsValid,
-      *SigmaXTrkVsHitsGood, *SigmaXTrkVsHitsInvalid, *SigmaXTrkVsLayersMissed, *SigmaXVsNorChi2,
-      *SigmaXHitVsPhiSens, *SigmaXHitVsPhiSensX, *SigmaXHitVsPhiSensY,
-      *SigmaXTrkVsPhiSens, *SigmaXTrkVsPhiSensX, *SigmaXTrkVsPhiSensY,
-      *SigmaXVsPhiSens, *SigmaXVsPhiSensX, *SigmaXVsPhiSensY,
-      *NorResXVsSigmaXHit, *NorResXVsSigmaXTrk, *NorResXVsSigmaX,
-      *WidthVsPhiSensX;
-  TProfile *PNorResXVsWidth, *PNorResXVsMaxStrip, *PNorResXVsCharge, *PNorResXVsMaxCharge, *PNorResXVsMaxIndex, *PNorResXVsChargeLR,
-           *PNorResXVsBaryStrip, *PNorResXVsSOverN,
-           *PNorResXVsP, *PNorResXVsMeanAngle, *PNorResXVsHitsValid, *PNorResXVsHitsGood,
-           *PNorResXVsHitsInvalid, *PNorResXVsLayersMissed, *PNorResXVsNorChi2,
-	   *PNorResXVsPhiSens, *PNorResXVsPhiSensX, *PNorResXVsPhiSensY,
-	   *PSigmaXHitVsWidth, *PSigmaXHitVsMaxStrip, *PSigmaXHitVsCharge, *PSigmaXHitVsMaxCharge, *PSigmaXHitVsMaxIndex, *PSigmaXHitVsChargeLR,
-           *PSigmaXHitVsBaryStrip, *PSigmaXHitVsSOverN,
-	   *PSigmaXTrkVsP, *PSigmaXTrkVsInvP, *PSigmaXTrkVsMeanAngle, *PSigmaXTrkVsHitsValid,
-           *PSigmaXTrkVsHitsGood, *PSigmaXTrkVsHitsInvalid, *PSigmaXTrkVsLayersMissed, *PSigmaXVsNorChi2,
-	   *PSigmaXHitVsPhiSens, *PSigmaXHitVsPhiSensX, *PSigmaXHitVsPhiSensY,
-           *PSigmaXTrkVsPhiSens, *PSigmaXTrkVsPhiSensX, *PSigmaXTrkVsPhiSensY,
-	   *PSigmaXVsPhiSens, *PSigmaXVsPhiSensX, *PSigmaXVsPhiSensY,
-	   *PNorResXVsSigmaXHit, *PNorResXVsSigmaXTrk, *PNorResXVsSigmaX,
-	   *PWidthVsPhiSensX;
+  float apeX;
+  
+  TH1 *ResX, *NorResX, *XHit, *XTrk,
+      *SigmaX2, *ProbX;
+  TH2 *WidthVsPhiSensX, *WidthVsWidthProjected, *WidthDiffVsMaxStrip;
+  TProfile *PWidthVsPhiSensX, *PWidthVsWidthProjected, *PWidthDiffVsMaxStrip;
   
   std::map<std::string,std::vector<TH1*> > mSigmaX;
   
@@ -90,8 +86,167 @@ struct TrackerSectorStruct{
   
   
   //for presenting results
-  TH1 *Entries, *MeanX, *RmsX, *FitMeanX, *ResidualWidthX, *ApeX;
+  TH1 *Entries, *MeanX, *RmsX, *FitMeanX1, *ResidualWidthX1, *ApeX1,
+      *FitMeanX2, *ResidualWidthX2, *ApeX2;
   
 };
+
+
+
+
+
+
+
+void
+TrackerSectorStruct::setCorrHistParams(TFileDirectory *directory, double norResXMax, double sigmaXHitMax, double sigmaXMax){
+  directory_ = directory;
+  norResXMax_ = norResXMax;
+  sigmaXHitMax_ = sigmaXHitMax;
+  sigmaXMax_ = sigmaXMax;
+}
+
+
+
+TrackerSectorStruct::CorrelationHists
+TrackerSectorStruct::bookCorrHists(TString varName,TString varTitle,TString labelX,TString unitX,int nBinX1D,int nBinX2D,double minBinX,double maxBinX,std::string options){
+  
+  std::string o(options);
+  CorrelationHists correlationHists;
+  
+  if(!(o.find("n") != std::string::npos || o.find("p") != std::string::npos || o.find("h") != std::string::npos || 
+       o.find("t") != std::string::npos || o.find("r") != std::string::npos))return correlationHists;
+  
+  TFileDirectory *directory(directory_);
+  double norResXMax(norResXMax_), sigmaXHitMax(sigmaXHitMax_), sigmaXMax(sigmaXMax_);
+  
+  if(!directory)return correlationHists;
+  
+  
+  correlationHists.Variable = directory->make<TH1F>("h_"+varName,varTitle+" "+labelX+";"+labelX+"  "+unitX+";# hits",nBinX1D,minBinX,maxBinX);
+  
+  if(options.find("n") != std::string::npos)
+  correlationHists.NorResXVsVar = directory->make<TH2F>("h2_norResXVs"+varName,"r_{x}/#sigma_{x} vs. "+labelX+";"+labelX+"  "+unitX+";(x_{track}-x_{hit})'/#sigma_{x}",nBinX2D,minBinX,maxBinX,25,-norResXMax,norResXMax);
+  if(options.find("p") != std::string::npos)
+  correlationHists.ProbXVsVar = directory->make<TH2F>("h2_probXVs"+varName,"prob_{x} vs. "+labelX+";"+labelX+"  "+unitX+";prob_{x}",nBinX2D,minBinX,maxBinX,60,-0.1,1.1);
+  if(options.find("h") != std::string::npos)
+  correlationHists.SigmaXHitVsVar = directory->make<TH2F>("h2_sigmaXHitVs"+varName,"#sigma_{x,hit} vs. "+labelX+";"+labelX+"  "+unitX+";#sigma_{x,hit}  [cm]",nBinX2D,minBinX,maxBinX,50,0,sigmaXHitMax);
+  if(options.find("t") != std::string::npos)
+  correlationHists.SigmaXTrkVsVar = directory->make<TH2F>("h2_sigmaXTrkVs"+varName,"#sigma_{x,track} vs. "+labelX+";"+labelX+"  "+unitX+";#sigma_{x,track}  [cm]",nBinX2D,minBinX,maxBinX,50,0,sigmaXMax);
+  if(options.find("r") != std::string::npos)
+  correlationHists.SigmaXVsVar = directory->make<TH2F>("h2_sigmaXVs"+varName,"#sigma_{x} vs. "+labelX+";"+labelX+"  "+unitX+";#sigma_{x}  [cm]",nBinX2D,minBinX,maxBinX,50,0,sigmaXMax);
+    
+  if(options.find("n") != std::string::npos)
+  correlationHists.PNorResXVsVar = directory->make<TProfile>("p_norResXVs"+varName,"r_{x}/#sigma_{x} vs. "+labelX+";"+labelX+"  "+unitX+";(x_{track}-x_{hit})'/#sigma_{x}",nBinX2D,minBinX,maxBinX,"s");
+  if(options.find("p") != std::string::npos)
+  correlationHists.PProbXVsVar = directory->make<TProfile>("p_probXVs"+varName,"prob_{x} vs. "+labelX+";"+labelX+"  "+unitX+";prob_{x}",nBinX2D,minBinX,maxBinX,"s");
+  if(options.find("h") != std::string::npos)
+  correlationHists.PSigmaXHitVsVar = directory->make<TProfile>("p_sigmaXHitVs"+varName,"#sigma_{x,hit} vs. "+labelX+";"+labelX+"  "+unitX+";#sigma_{x,hit}  [cm]",nBinX2D,minBinX,maxBinX,"s");
+  if(options.find("t") != std::string::npos)
+  correlationHists.PSigmaXTrkVsVar = directory->make<TProfile>("p_sigmaXTrkVs"+varName,"#sigma_{x,track} vs. "+labelX+";"+labelX+"  "+unitX+";#sigma_{x,track}  [cm]",nBinX2D,minBinX,maxBinX,"s");
+  if(options.find("r") != std::string::npos)
+  correlationHists.PSigmaXVsVar = directory->make<TProfile>("p_sigmaXVs"+varName,"#sigma_{x} vs. "+labelX+";"+labelX+"  "+unitX+";#sigma_{x}  [cm]",nBinX2D,minBinX,maxBinX,"s");
+  
+  return correlationHists;
+}
+
+
+TrackerSectorStruct::CorrelationHists
+TrackerSectorStruct::bookCorrHists(TString varName,TString labelX,TString unitX,int nBinX,double minBinX,double maxBinX,std::string options){
+  
+  std::string o(options);
+  CorrelationHists correlationHists;
+  
+  if(!(o.find("n") != std::string::npos || o.find("p") != std::string::npos || o.find("h") != std::string::npos || 
+       o.find("t") != std::string::npos || o.find("r") != std::string::npos))return correlationHists;
+  
+  TFileDirectory *directory(directory_);
+  double norResXMax(norResXMax_), sigmaXHitMax(sigmaXHitMax_), sigmaXMax(sigmaXMax_);
+  
+  if(!directory)return correlationHists;
+  
+  
+  if(options.find("n") != std::string::npos)
+  correlationHists.NorResXVsVar = directory->make<TH2F>("h2_norResXVs"+varName,"r_{x}/#sigma_{x} vs. "+labelX+";"+labelX+"  "+unitX+";(x_{track}-x_{hit})'/#sigma_{x}",nBinX,minBinX,maxBinX,25,-norResXMax,norResXMax);
+  if(options.find("p") != std::string::npos)
+  correlationHists.ProbXVsVar = directory->make<TH2F>("h2_probXVs"+varName,"prob_{x} vs. "+labelX+";"+labelX+"  "+unitX+";prob_{x}",nBinX,minBinX,maxBinX,60,-0.1,1.1);
+  if(options.find("h") != std::string::npos)
+  correlationHists.SigmaXHitVsVar = directory->make<TH2F>("h2_sigmaXHitVs"+varName,"#sigma_{x,hit} vs. "+labelX+";"+labelX+"  "+unitX+";#sigma_{x,hit}  [cm]",nBinX,minBinX,maxBinX,50,0,sigmaXHitMax);
+  if(options.find("t") != std::string::npos)
+  correlationHists.SigmaXTrkVsVar = directory->make<TH2F>("h2_sigmaXTrkVs"+varName,"#sigma_{x,track} vs. "+labelX+";"+labelX+"  "+unitX+";#sigma_{x,track}  [cm]",nBinX,minBinX,maxBinX,50,0,sigmaXMax);
+  if(options.find("r") != std::string::npos)
+  correlationHists.SigmaXVsVar = directory->make<TH2F>("h2_sigmaXVs"+varName,"#sigma_{x} vs. "+labelX+";"+labelX+"  "+unitX+";#sigma_{x}  [cm]",nBinX,minBinX,maxBinX,50,0,sigmaXMax);
+    
+  if(options.find("n") != std::string::npos)
+  correlationHists.PNorResXVsVar = directory->make<TProfile>("p_norResXVs"+varName,"r_{x}/#sigma_{x} vs. "+labelX+";"+labelX+"  "+unitX+";(x_{track}-x_{hit})'/#sigma_{x}",nBinX,minBinX,maxBinX,"s");
+  if(options.find("p") != std::string::npos)
+  correlationHists.PProbXVsVar = directory->make<TProfile>("p_probXVs"+varName,"prob_{x} vs. "+labelX+";"+labelX+"  "+unitX+";prob_{x}",nBinX,minBinX,maxBinX,"s");
+  if(options.find("h") != std::string::npos)
+  correlationHists.PSigmaXHitVsVar = directory->make<TProfile>("p_sigmaXHitVs"+varName,"#sigma_{x,hit} vs. "+labelX+";"+labelX+"  "+unitX+";#sigma_{x,hit}  [cm]",nBinX,minBinX,maxBinX);
+  if(options.find("t") != std::string::npos)
+  correlationHists.PSigmaXTrkVsVar = directory->make<TProfile>("p_sigmaXTrkVs"+varName,"#sigma_{x,track} vs. "+labelX+";"+labelX+"  "+unitX+";#sigma_{x,track}  [cm]",nBinX,minBinX,maxBinX);
+  if(options.find("r") != std::string::npos)
+  correlationHists.PSigmaXVsVar = directory->make<TProfile>("p_sigmaXVs"+varName,"#sigma_{x} vs. "+labelX+";"+labelX+"  "+unitX+";#sigma_{x}  [cm]",nBinX,minBinX,maxBinX);
+  
+  return correlationHists;
+}
+
+void
+TrackerSectorStruct::CorrelationHists::fillCorrHists(const TrackStruct::HitParameterStruct& hitParameterStruct, double variable){
+  
+  if(Variable){Variable->Fill(variable);}
+  
+  if(NorResXVsVar){
+    NorResXVsVar->Fill(variable,hitParameterStruct.norResX);
+    PNorResXVsVar->Fill(variable,hitParameterStruct.norResX);
+  }
+  if(ProbXVsVar){
+    ProbXVsVar->Fill(variable,hitParameterStruct.probX);
+    PProbXVsVar->Fill(variable,hitParameterStruct.probX);
+  }
+  if(SigmaXHitVsVar){
+    SigmaXHitVsVar->Fill(variable,hitParameterStruct.errXHit);
+    PSigmaXHitVsVar->Fill(variable,hitParameterStruct.errXHit);
+  }
+  if(SigmaXTrkVsVar){
+    SigmaXTrkVsVar->Fill(variable,hitParameterStruct.errXTrk);
+    PSigmaXTrkVsVar->Fill(variable,hitParameterStruct.errXTrk);
+  }
+  if(SigmaXVsVar){
+    SigmaXVsVar->Fill(variable,hitParameterStruct.errX);
+    PSigmaXVsVar->Fill(variable,hitParameterStruct.errX);
+  }
+  
+}
+
+/*
+void
+TrackerSectorStruct::fillCorrHists(double variable, const TrackStruct::HitParameterStruct& hitParameterStruct, CorrelationHists& corrHists){
+  
+  if(corrHists.Variable){corrHists.Variable->Fill(variable);}
+  
+  if(corrHists.NorResXVsVar){
+    corrHists.NorResXVsVar->Fill(variable,hitParameterStruct.norResX);
+    corrHists.PNorResXVsVar->Fill(variable,hitParameterStruct.norResX);
+  }
+  if(corrHists.ProbXVsVar){
+    corrHists.ProbXVsVar->Fill(variable,hitParameterStruct.probX);
+    corrHists.PProbXVsVar->Fill(variable,hitParameterStruct.probX);
+  }
+  if(corrHists.SigmaXHitVsVar){
+    corrHists.SigmaXHitVsVar->Fill(variable,hitParameterStruct.errXHit);
+    corrHists.PSigmaXHitVsVar->Fill(variable,hitParameterStruct.errXHit);
+  }
+  if(corrHists.SigmaXTrkVsVar){
+    corrHists.SigmaXTrkVsVar->Fill(variable,hitParameterStruct.errXTrk);
+    corrHists.PSigmaXTrkVsVar->Fill(variable,hitParameterStruct.errXTrk);
+  }
+  if(corrHists.SigmaXVsVar){
+    corrHists.SigmaXVsVar->Fill(variable,hitParameterStruct.errX);
+    corrHists.PSigmaXVsVar->Fill(variable,hitParameterStruct.errX);
+  }
+  
+}
+*/
+
 
 #endif
