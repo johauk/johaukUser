60193454bb83c20d5ae403abbcd9a82a6b7d8d5a
hauk <>
2010-06-16 15:49:05 +0000

    d0 histogram changed from wrt. (0,0,0) to wrt. Beamspot

--- ApeAndCpeStudies/FullVersion/ApeEstimator/ApeEstimator/interface/TrackerDetectorStruct.h
+++ ApeAndCpeStudies/FullVersion/ApeEstimator/ApeEstimator/interface/TrackerDetectorStruct.h
@@ -10,7 +10,7 @@ struct TrackerDetectorStruct{
                            HitsSize(0), HitsValid(0), HitsInvalid(0), Hits2D(0),
 			   HitsGood(0), LayersMissed(0),
 			   Charge(0), Chi2(0), Ndof(0), NorChi2(0),
-			   Eta(0), Theta(0), Phi(0), D0(0), Dz(0),
+			   Eta(0), Theta(0), Phi(0), D0Beamspot(0), Dz(0),
 			   P(0), Pt(0), MeanAngle(0),
 			   HitsGoodVsHitsValid(0), MeanAngleVsHits(0),
 			   PHitsGoodVsHitsValid(0), PMeanAngleVsHits(0){}
@@ -19,7 +19,7 @@ struct TrackerDetectorStruct{
       *HitsSize, *HitsValid, *HitsInvalid, *Hits2D,
       *HitsGood, *LayersMissed,
       *Charge, *Chi2, *Ndof, *NorChi2,
-      *Eta, *Theta, *Phi, *D0, *Dz,
+      *Eta, *Theta, *Phi, *D0Beamspot, *Dz,
       *P, *Pt, *MeanAngle;
   
   TH2 *HitsGoodVsHitsValid, *MeanAngleVsHits;
