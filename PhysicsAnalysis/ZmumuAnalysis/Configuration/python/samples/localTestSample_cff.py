73b1b1870190e1290c12ceb539f1884f276e43d7
hauk <>
2010-06-23 10:19:49 +0000

    Use locally stored test file as source

---
+++ PhysicsAnalysis/ZmumuAnalysis/Configuration/python/samples/localTestSample_cff.py
@@ -0,0 +1,13 @@
+import FWCore.ParameterSet.Config as cms
+
+readFiles = cms.untracked.vstring()
+source = cms.Source ("PoolSource",fileNames = readFiles )
+
+readFiles.extend( [
+
+
+'file:/scratch/hh/current/cms/user/hauk/data/testPatTuple/PATtuple_2.root',
+
+
+
+] );
