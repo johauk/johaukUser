a875e40bdef47661c80c8e18585701670df2312c
hauk <>
2010-06-23 10:18:10 +0000

    Adaption to use locally stored test file

--- PhysicsAnalysis/ZmumuAnalysis/Configuration/python/samples/testSample_cff.py
+++ PhysicsAnalysis/ZmumuAnalysis/Configuration/python/samples/testSample_cff.py
@@ -1,3 +1,7 @@
-# ideally should be simulated Z->mumu sample to allow testing of all modules
+## ideally should be simulated Z->mumu sample to allow testing of all modules
 
-from ZmumuAnalysis.Configuration.samples.mc.Spring10.Zmumu_Pat_cff import source
+## link to files on dCache
+#from ZmumuAnalysis.Configuration.samples.mc.Spring10.Zmumu_Pat_cff import source
+
+## link to locally stored small test file
+from ZmumuAnalysis.Configuration.samples.localTestSample_cff import source
