6c9afeaf6c26ec606f76540787479f8c85ca2d49
hauk <>
2010-06-23 13:13:32 +0000

    Filters for charge combinations added

--- PhysicsAnalysis/ZmumuAnalysis/Configuration/python/filters/DiMuonFilters_cff.py
+++ PhysicsAnalysis/ZmumuAnalysis/Configuration/python/filters/DiMuonFilters_cff.py
@@ -4,6 +4,18 @@ import ZmumuAnalysis.Filter.DiMuonFilter_cfi
 
 
 
+DiMuonOppositeChargeFilter = ZmumuAnalysis.Filter.DiMuonFilter_cfi.DiMuonFilter.clone(
+    allowSameCharge = False,
+)
+
+
+
+DiMuonSameChargeFilter = ZmumuAnalysis.Filter.DiMuonFilter_cfi.DiMuonFilter.clone(
+    allowOppositeCharge = False,
+)
+
+
+
 DiMuonMassFilter = ZmumuAnalysis.Filter.DiMuonFilter_cfi.DiMuonFilter.clone(
     massIntervals = [80.,100.],
 )
