15679b8fbb87b5b7f55da61c49bffe624e444737
hauk <>
2010-07-02 11:33:39 +0000

    Cleanup

--- ApeAndCpeStudies/FullVersion/ApeEstimator/ApeEstimator/python/PrivateSkim_EventContent_cff.py
+++ ApeAndCpeStudies/FullVersion/ApeEstimator/ApeEstimator/python/PrivateSkim_EventContent_cff.py
@@ -3,19 +3,6 @@ import FWCore.ParameterSet.Config as cms
 
 SimulationEventContent = cms.PSet(
     outputCommands = cms.untracked.vstring(
-        #'drop  recoGenJets_*_*_RAW',
-	#'drop  recoGenMETs_*_*_RAW',
-	#'drop  recoGenParticles_*_*_RAW',
-	#'drop  Sim*_g4SimHits_*_RAW',
-	#'drop  P*Hits_g4SimHits_*_RAW',
-	#'keep  PSimHits_g4SimHits_TrackerHits*_RAW',
-	#'drop  *_simMuon*Digis_*_RAW',
-	#'drop  CrossingFramePlaybackInfoExtended_mix__RAW',
-	#'drop  GenEventInfoProduct_generator__RAW',
-	#'drop  edm*_*_*_RAW',
-	#'drop  RandomEngineStates_randomEngineStateProducer__RAW',
-	#'drop  ints_genParticles__RAW',
-	
 	'drop  *_*_*_RAW',
 	'keep  PSimHits_g4SimHits_TrackerHits*_RAW',
 	'keep  FEDRawDataCollection_rawDataCollector__RAW',
