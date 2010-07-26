b7d2f86302e2bb00285fb7fe574aaafc8916de07
hauk <>
2010-07-26 12:27:00 +0000

    Use full statistics of private particle gun simulation

--- ApeAndCpeStudies/FullVersion/ApeEstimator/ApeEstimator/python/samples/ParticleGunPion_mc_cff.py
+++ ApeAndCpeStudies/FullVersion/ApeEstimator/ApeEstimator/python/samples/ParticleGunPion_mc_cff.py
@@ -9,9 +9,17 @@ readFiles.extend( [
 
 
 
-#'rfio:///?svcClass=cmscafuser&path=/castor/cern.ch/cms/store/caf/user/hauk/mc/MinBias_START3X_V26_S09_356.root',
-'file:/afs/naf.desy.de/user/j/johauk/cms/testing/CMSSW_3_5_6/src/ApeEstimator/ApeEstimator/test/ParticleGun/reco.root',
-
+#'file:/afs/naf.desy.de/user/j/johauk/cms/testing/CMSSW_3_5_6/src/ApeEstimator/ApeEstimator/test/ParticleGun/reco.root',
+'rfio:///?svcClass=cmscafuser&path=/castor/cern.ch/cms/store/caf/user/hauk/mc/ParticleGunPion/RECO/reco1.root',
+'rfio:///?svcClass=cmscafuser&path=/castor/cern.ch/cms/store/caf/user/hauk/mc/ParticleGunPion/RECO/reco2.root',
+'rfio:///?svcClass=cmscafuser&path=/castor/cern.ch/cms/store/caf/user/hauk/mc/ParticleGunPion/RECO/reco3.root',
+'rfio:///?svcClass=cmscafuser&path=/castor/cern.ch/cms/store/caf/user/hauk/mc/ParticleGunPion/RECO/reco4.root',
+'rfio:///?svcClass=cmscafuser&path=/castor/cern.ch/cms/store/caf/user/hauk/mc/ParticleGunPion/RECO/reco5.root',
+'rfio:///?svcClass=cmscafuser&path=/castor/cern.ch/cms/store/caf/user/hauk/mc/ParticleGunPion/RECO/reco6.root',
+'rfio:///?svcClass=cmscafuser&path=/castor/cern.ch/cms/store/caf/user/hauk/mc/ParticleGunPion/RECO/reco7.root',
+'rfio:///?svcClass=cmscafuser&path=/castor/cern.ch/cms/store/caf/user/hauk/mc/ParticleGunPion/RECO/reco8.root',
+'rfio:///?svcClass=cmscafuser&path=/castor/cern.ch/cms/store/caf/user/hauk/mc/ParticleGunPion/RECO/reco9.root',
+'rfio:///?svcClass=cmscafuser&path=/castor/cern.ch/cms/store/caf/user/hauk/mc/ParticleGunPion/RECO/reco10.root',
 
 
 ] );
