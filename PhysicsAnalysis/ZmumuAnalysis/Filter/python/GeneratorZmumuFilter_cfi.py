3fc0cb446c7821334457558dac4b8f6012b0a680
hauk <>
2010-09-24 14:55:43 +0000

    Extend generator filter from Z->mumu to configurable Z->xx More than one decay channel at same time possible

--- PhysicsAnalysis/ZmumuAnalysis/Filter/python/GeneratorZmumuFilter_cfi.py
+++ PhysicsAnalysis/ZmumuAnalysis/Filter/python/GeneratorZmumuFilter_cfi.py
@@ -4,7 +4,12 @@ GeneratorZmumuFilter = cms.EDFilter('GeneratorZmumuFilter',
     # generator particle collection
     src = cms.InputTag("genParticles"),
     
-    ## Default: all filters inactivated, but filter for at least one Z->mumu
+    
+    ## Default: all filters inactivated, but filter for at least one Z->xx
+    # filter on Z decay mode via PDG-id: default is muon(13)
+    zDecayMode = cms.vint32(13),  
+    
+    # all variable names are like in Z->mumu case, but act universally on Z->xx
     
     ## Cut on Z/dimuon properties
     # filter on eta difference
