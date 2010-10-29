a61fca8a66ce9d97348b065f0cf24c77aba06514
hauk <>
2010-10-29 16:58:34 +0000

    Reorganisation due to prescaling of HLT_Mu9 in data taking

--- PhysicsAnalysis/ZmumuAnalysis/Configuration/python/filters/TriggerFilter_cff.py
+++ PhysicsAnalysis/ZmumuAnalysis/Configuration/python/filters/TriggerFilter_cff.py
@@ -4,14 +4,28 @@ import HLTrigger.HLTfilters.hltHighLevel_cfi
 
 
 
-AllLowestUnprescaledTriggerFilter = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone(
-    #HLTPaths = ["HLT_Mu11","HLT_DoubleMu3","HLT_IsoMu9"],
-    HLTPaths = ["HLT_Mu9","HLT_DoubleMu3"],
-)
-
-
-
-FinalTriggerFilter =  HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone(
+TriggerFilterMu9 =  HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone(
     HLTPaths = ["HLT_Mu9"],
 )
 
+
+
+TriggerFilterMu9_11_15 =  HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone(
+    HLTPaths = ["HLT_Mu9","HLT_Mu11","HLT_Mu15_v*"],
+    throw = False,   # do not throw exception on events where not all listed triggers are in...
+)
+
+
+
+# different trigger versions (bla_v*) are only in data, does not select events on MC at present
+TriggerFilterMu15 =  HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone(
+    HLTPaths = ["HLT_Mu15_v*"],
+    throw = False,
+)
+
+
+
+# this would work in MC
+TriggerFilter_Mc_Mu15 =  HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone(
+    HLTPaths = ["HLT_Mu15"],
+)
