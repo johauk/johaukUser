20bd75edf8a5e3247e7bacb82ca2335bf0fb974e
hauk <>
2010-12-09 16:56:41 +0000

    Changed isolation of muons to combined relative isolation

--- PhysicsAnalysis/ZmumuAnalysis/Configuration/python/sequences/diMuonSelection_cff.py
+++ PhysicsAnalysis/ZmumuAnalysis/Configuration/python/sequences/diMuonSelection_cff.py
@@ -5,7 +5,8 @@ from ElectroWeakAnalysis.Skimming.dimuons_cfi import *
 ## di-muon candidate count filter
 from ElectroWeakAnalysis.Skimming.dimuonsFilter_cfi import *
 ## isolation selection
-from ElectroWeakAnalysis.ZMuMu.zSelection_cfi import *
+# Do not use anymore this IsoDeposit based parameters, use default PAT values
+#from ElectroWeakAnalysis.ZMuMu.zSelection_cfi import *
 
 
 
@@ -20,13 +21,24 @@ from ElectroWeakAnalysis.ZMuMu.zSelection_cfi import *
 ##
 
 # Collection for ambiguity cleaning using "ZMuMuOverlapExclusionSelector"
-tightHltGlobalDimuons = dimuons.clone(
-    checkCharge = False,
-    cut = 'daughter(0).isGlobalMuon = 1' +'&'+
-	  'daughter(1).isGlobalMuon = 1',
-    #decay = 'tightHltMuons@+ tightHltMuons@-',
-    #decay = 'tightMuonsTriggerMatch@+ tightMuonsTriggerMatch@-',
-    decay = 'tightHltMuonsTriggerMatch@+ tightHltMuonsTriggerMatch@-',
+#tightHltGlobalDimuons = dimuons.clone(
+#    checkCharge = False,
+#    cut = '',#daughter(0).isGlobalMuon = 1' +'&'+
+#	  #'daughter(1).isGlobalMuon = 1',
+#    #decay = 'tightHltMuons@+ tightHltMuons@-',
+#    #decay = 'tightMuonsTriggerMatch@+ tightMuonsTriggerMatch@-',
+#    decay = 'tightHltMuonsTriggerMatch@+ tightHltMuonsTriggerMatch@-',
+#)
+## One could use the "CandViewCombiner" as soon as it is made lazy (not yet in CMSSW_3_8_X) instead of the default "CandViewShallowCloneCombiner"
+## Then daughters know that they are of type pat::Muon and not of ShallowCloneCandidate!
+## But this is not needed since it knows again, when asking for the masterClone:
+##  pat::Muon = ShallowCloneCandidate.masterClone()
+## So always use the ShallowClone variant to use official tools in package ElectroWeakAnalysis/
+tightHltGlobalDimuons = cms.EDProducer("CandViewShallowCloneCombiner",
+#tightHltGlobalDimuons = cms.EDProducer("CandViewCombiner",
+    checkCharge = cms.bool(False),
+    cut = cms.string(''),
+    decay = cms.string('tightHltMuonsTriggerMatch@+ tightHltMuonsTriggerMatch@-'),
 )
 
 
@@ -56,29 +68,67 @@ selectedDimuons = cms.EDProducer("CandViewMerger",
 
 
 
-# Cuts on properties of Dimuon system
+# Cuts on properties of Dimuon system & Split opposite charge vs same charge
 goodDimuons = cms.EDFilter("CandViewRefSelector",
     src = cms.InputTag("selectedDimuons"),
     cut = cms.string(
-      'mass > 20.'
+      'mass > 20. &'
+      'charge = 0'
+    ),
+)
+goodDimuonsSC = cms.EDFilter("CandViewRefSelector",
+    src = cms.InputTag("selectedDimuons"),
+    cut = cms.string(
+      'mass > 20. &'
+      'charge != 0'
     ),
 )
 
 
 
 # Cut on isolation of both Muons in Dimuon system
-diMuonIsolation = zSelection.clone(
-  cut = '',
-)
-isolatedDimuons = cms.EDFilter("ZToMuMuIsolatedIDSelector",
-    diMuonIsolation,
+## Do not use this complicated module based on IsoDeposits -- easily causes mistakes
+#diMuonIsolation = zSelection.clone(
+#  cut = '',
+#  isoCut = 3.,
+#)
+#isolatedDimuons = cms.EDFilter("ZToMuMuIsolatedIDSelector",
+#    diMuonIsolation,
+#    src = cms.InputTag("goodDimuons"),
+#    filter = cms.bool(False)
+#)
+## Instead use this module as long as it works (no combined and no relative isolation!!! - so, tracker only or calo only...)
+## But does it really work as expected??? --- no event is deselected!!!
+#isolatedDimuons = cms.EDFilter("ZToMuMuIsolatedSelector",
+#    src = cms.InputTag("goodDimuons"),
+#    cut = cms.string(''),
+#    isoCut = cms.double(3.),
+#    isolationType = cms.string("track"),
+#    filter = cms.bool(False)
+#)
+## Do the isolation cuts by hand in simple way
+## Use the "CandViewSelector" and not the "CandViewRefSelector":
+## First one is a lazy parser, so knows about input type
+## Here: it is a ShallowCloneCandidate with corresponding methods and not only a Candidate*, as the plugin thinks as it is its input
+isolatedDimuons = cms.EDFilter("CandViewSelector",
     src = cms.InputTag("goodDimuons"),
-    filter = cms.bool(False)
+    cut = cms.string(
+      '((daughter(0).masterClone.trackIso + daughter(0).masterClone.caloIso) /daughter(0).masterClone.pt < 0.15) &'
+      '((daughter(1).masterClone.trackIso + daughter(1).masterClone.caloIso) /daughter(1).masterClone.pt < 0.15)'
+    ),
 )
+## Or use directly this method (Unfortunately works only on reco::Muon and not on pat::Muon)
+#isolatedDimuons = cms.EDFilter("CompositeDimuonAndSelector",
+#    src = cms.InputTag("goodDimuons"),
+#    daughter1cut = cms.string('(isolationR03.sumPt + isolationR03.emEt + isolationR03.hadEt) /pt < 0.15'),
+#    daughter2cut = cms.string('(isolationR03.sumPt + isolationR03.emEt + isolationR03.hadEt) /pt < 0.15'),
+#)
+isolatedDimuonsSC = isolatedDimuons.clone(src = "goodDimuonsSC")
 
 
 
 # Check for HLT matching of at least one of both Muons
+## Works only with 
 atLeast1HltDimuons = cms.EDFilter("ZHLTMatchFilter",
     src = cms.InputTag("isolatedDimuons"),
     condition =cms.string("atLeastOneMatched"),
@@ -88,18 +138,22 @@ atLeast1HltDimuons = cms.EDFilter("ZHLTMatchFilter",
     hltPath = cms.string("HLT_Mu9"),
     filter = cms.bool(False) 
 )
+atLeast1HltDimuonsSC = atLeast1HltDimuons.clone(src = "isolatedDimuonsSC")
 
 
 
 # Final cuts on dimuon properties
 finalDimuons = cms.EDFilter("CandViewRefSelector",
     src = cms.InputTag("isolatedDimuons"),
+    #src = cms.InputTag("atLeast1HltDimuons"),
     cut = cms.string(
       'mass > 60.' +'&'+
       'mass < 120.'# +'&'+
       #'charge = 0'# +'&'+
     ),
 )
+finalDimuonsSC = finalDimuons.clone(src = "isolatedDimuonsSC")
+#finalDimuonsSC = finalDimuons.clone(src = "atLeast1HltDimuonsSC")
 
 
 
@@ -139,6 +193,13 @@ finalDimuonSelection = dimuonsFilter.clone(
 
 
 
+goodDimuonSCSelection = goodDimuonSelection.clone(src = 'goodDimuonsSC')
+isolatedDimuonSCSelection = isolatedDimuonSelection.clone(src = 'isolatedDimuonsSC')
+atLeast1HltDimuonSCSelection = atLeast1HltDimuonSelection.clone(src = 'atLeast1HltDimuonsSC')
+finalDimuonSCSelection = finalDimuonSelection.clone(src = 'finalDimuonsSC')
+
+
+
 
 
 ###########################################################################################
@@ -156,8 +217,22 @@ buildDimuonCollections = cms.Sequence(
     *selectedDimuons
     *goodDimuons
     *isolatedDimuons
-    *atLeast1HltDimuons
+#    *atLeast1HltDimuons
     *finalDimuons
+)
+
+
+
+buildDimuonSCCollections = cms.Sequence(
+    tightHltGlobalDimuons
+    *looseTightHltGlobalDimuons
+    *overlapExcludedLooseTightHltGlobalDimuons
+    *selectedDimuons
+    
+    *goodDimuonsSC
+    *isolatedDimuonsSC
+#    *atLeast1HltDimuonsSC
+    *finalDimuonsSC
 )
 
 
@@ -166,8 +241,18 @@ dimuonSelection = cms.Sequence(
     selectedDimuonSelection
     *goodDimuonSelection
     *isolatedDimuonSelection
-    *atLeast1HltDimuonSelection
+#    *atLeast1HltDimuonSelection
     *finalDimuonSelection
+)
+
+
+
+dimuonSCSelection = cms.Sequence(
+    selectedDimuonSelection
+    *goodDimuonSCSelection
+    *isolatedDimuonSCSelection
+#    *atLeast1HltDimuonSCSelection
+    *finalDimuonSCSelection
 )
 
 
