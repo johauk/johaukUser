7cf3d6ce78b48c9f14b654f3173f94a17667e0b9
hauk <>
2010-12-09 17:54:23 +0000

    Split analysis for same charge and opposite charge dimuons

--- PhysicsAnalysis/ZmumuAnalysis/Configuration/analysis/mc/singleTopTW_cfg.py
+++ PhysicsAnalysis/ZmumuAnalysis/Configuration/analysis/mc/singleTopTW_cfg.py
@@ -2,13 +2,12 @@ import os
 
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("DiMuonSelectionCutflow")
+process = cms.Process("FullCutflow")
 
 ## add message logger
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.MessageLogger.cerr.threshold = 'INFO'
 process.MessageLogger.cerr.FwkReport.reportEvery = 1000
-process.MessageLogger.suppressWarning = cms.untracked.vstring("decaySubset") 
 
 
 
@@ -22,23 +21,35 @@ process.maxEvents = cms.untracked.PSet(
 ## configure process options
 process.options = cms.untracked.PSet(
     wantSummary = cms.untracked.bool(True),
-    #SkipEvent = cms.untracked.vstring('ProductNotFound'),
-    #Rethrow = cms.untracked.vstring("ProductNotFound"), # make this exception fatal (now default?)
 )
 
 
 
 ## sources
+#process.load("ZmumuAnalysis.Configuration.samples.mc.Spring10.samples.Run2010A_Sep17ReReco_v2_Oct22_cff")
+#process.load("ZmumuAnalysis.Configuration.samples.mc.Spring10.samples.Run2010B_PromptReco_v2_Oct29_145000_147120_cff")
+#process.load("ZmumuAnalysis.Configuration.samples.mc.Spring10.samples.Run2010B_PromptReco_v2_Oct29_147120_148864_cff")
+#process.load("ZmumuAnalysis.Configuration.samples.mc.Spring10.samples.inclusiveMu15_spring10_cff")
+#process.load("ZmumuAnalysis.Configuration.samples.mc.Spring10.samples.singleTopS_spring10_cff")
 process.load("ZmumuAnalysis.Configuration.samples.mc.Spring10.samples.singleTopTW_spring10_cff")
+#process.load("ZmumuAnalysis.Configuration.samples.mc.Spring10.samples.singleTopT_spring10_cff")
+#process.load("ZmumuAnalysis.Configuration.samples.mc.Spring10.samples.ttbar_spring10_cff")
+#process.load("ZmumuAnalysis.Configuration.samples.mc.Spring10.samples.wmunu_spring10_cff")
+#process.load("ZmumuAnalysis.Configuration.samples.mc.Spring10.samples.zmumu_spring10_cff")
+#process.load("ZmumuAnalysis.Configuration.samples.mc.Spring10.samples.ztautau_spring10_cff")
+#process.load("ZmumuAnalysis.Configuration.samples.mc.Spring10.samples.ww_spring10_cff")
+#process.load("ZmumuAnalysis.Configuration.samples.mc.Spring10.samples.wz_spring10_cff")
+#process.load("ZmumuAnalysis.Configuration.samples.mc.Spring10.samples.zz_spring10_cff")
+#process.load("ZmumuAnalysis.Configuration.samples.testSample_cff")
 
 
 
 ## needed for access to trigger menu
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 # data
-#process.GlobalTag.globaltag = cms.string('GR_R_36X_V12::All')
+#process.GlobalTag.globaltag = cms.string('GR_R_38X_V13::All')
 # mc
-process.GlobalTag.globaltag = cms.string('START36_V10::All')
+process.GlobalTag.globaltag = cms.string('START38_V12::All')
 
 
 
@@ -54,7 +65,8 @@ process.TFileService = cms.Service("TFileService",
 
 ## filter trigger
 process.load("ZmumuAnalysis.Configuration.filters.TriggerFilter_cff")
-process.triggerFilter1 = process.AllLowestUnprescaledTriggerFilter.clone()
+#process.triggerFilter1 = process.TriggerFilterMu9.clone()
+process.triggerFilter1 = process.TriggerFilterMu9_11_15.clone()
 
 
 
@@ -65,7 +77,12 @@ process.load("ZmumuAnalysis.Configuration.sequences.muonSelection_cff")
 
 ## add dimuon collections and filter for dimuon properties (including muon isolation)
 process.load("ZmumuAnalysis.Configuration.sequences.diMuonSelection_cff")
-#process.looseTightHltGlobalDimuons.decay = 'tightHltMuons@+ tightMuons@-'
+
+
+
+## clean and filter for jets
+process.load("ZmumuAnalysis.Configuration.sequences.jetSelection_cff")
+
 
 
 
@@ -79,37 +96,82 @@ process.TriggerAnalyzer1 =  process.TriggerAnalyzer.clone()
 
 
 
-## event analyzer
-process.load("ZmumuAnalysis.Analyzer.EventAnalyzer_cfi")
-process.EventAnalyzer1 = process.EventAnalyzer.clone(
-    muonSource = 'looseMuons',
-)
-process.EventAnalyzer2 = process.EventAnalyzer1.clone(
-    analyzeDiMuons = True,
-)
-process.EventAnalyzer3 = process.EventAnalyzer2.clone(
-    diMuonSource = "finalDimuons",
-)
-
-
-
 ## muon analizer
 process.load("ZmumuAnalysis.Analyzer.MuonAnalyzer_cfi")
 process.MuonAnalyzer1 = process.MuonAnalyzer.clone(
+    muonSource = 'looseMuons',
 )
 process.MuonAnalyzer2 = process.MuonAnalyzer1.clone(
-    muonSource = 'looseMuons',
+)
+process.MuonAnalyzer3 = process.MuonAnalyzer1.clone(
+)
+process.MuonAnalyzer4 = process.MuonAnalyzer1.clone(
+)
+process.MuonAnalyzer5 = process.MuonAnalyzer1.clone(
+)
+process.MuonAnalyzer6 = process.MuonAnalyzer1.clone(
+)
+process.MuonAnalyzer9 = process.MuonAnalyzer1.clone(
+)
+
+process.MuonSCAnalyzer6 = process.MuonAnalyzer6.clone(
+)
+process.MuonSCAnalyzer9 = process.MuonAnalyzer9.clone(
 )
 
 
 
 ## di-muon analyzer
 process.load("ZmumuAnalysis.Analyzer.DiMuonAnalyzer_cfi")
-process.DiMuonAnalyzer1 = process.DiMuonAnalyzer.clone(
+process.DiMuonAnalyzer3 = process.DiMuonAnalyzer.clone(
+    diMuonSource = "goodDimuons",
 )
-process.DiMuonAnalyzer2 = process.DiMuonAnalyzer1.clone(
+process.DiMuonAnalyzer4 = process.DiMuonAnalyzer.clone(
+    diMuonSource = "isolatedDimuons",
+)
+#process.DiMuonAnalyzer5 = process.DiMuonAnalyzer.clone(
+#    diMuonSource = "atLeast1HltDimuons",
+#)
+process.DiMuonAnalyzer6 = process.DiMuonAnalyzer.clone(
     diMuonSource = "finalDimuons",
 )
+process.DiMuonAnalyzer9 = process.DiMuonAnalyzer.clone(
+    diMuonSource = "finalDimuons",
+)
+
+process.DiMuonSCAnalyzer3 = process.DiMuonAnalyzer3.clone(
+    diMuonSource = "goodDimuonsSC",
+)
+process.DiMuonSCAnalyzer4 = process.DiMuonAnalyzer4.clone(
+    diMuonSource = "isolatedDimuonsSC",
+)
+#process.DiMuonSCAnalyzer5 = process.DiMuonAnalyzer5.clone(
+#    diMuonSource = "atLeast1HltDimuonsSC",
+#)
+process.DiMuonSCAnalyzer6 = process.DiMuonAnalyzer6.clone(
+    diMuonSource = "finalDimuonsSC",
+)
+process.DiMuonSCAnalyzer9 = process.DiMuonAnalyzer9.clone(
+    diMuonSource = "finalDimuonsSC",
+)
+
+
+
+## jet analyzer
+process.load("ZmumuAnalysis.Analyzer.JetAnalyzer_cfi")
+process.JetAnalyzer7 = process.JetAnalyzer.clone(
+    jetSource = 'finalJets',
+)
+process.JetAnalyzer8 = process.JetAnalyzer.clone(
+    jetSource = 'bTcheJets',
+)
+process.JetAnalyzer9 = process.JetAnalyzer.clone(
+    jetSource = 'bTcheJets',
+)
+
+process.JetSCAnalyzer7 = process.JetAnalyzer.clone()
+process.JetSCAnalyzer8 = process.JetAnalyzer.clone()
+process.JetSCAnalyzer9 = process.JetAnalyzer.clone()
 
 
 
@@ -121,20 +183,56 @@ process.DiMuonAnalyzer2 = process.DiMuonAnalyzer1.clone(
 
 
 
-process.p = cms.Path(
+process.oppositeChargeAnalysis = cms.Path(
     process.TriggerAnalyzer1
     *process.triggerFilter1
     
-    *process.MuonAnalyzer1
     *process.buildMuonCollections
-    *process.EventAnalyzer1
+    *process.MuonAnalyzer1
     *process.muonSelection
     *process.MuonAnalyzer2
     
     *process.buildDimuonCollections
-    *process.EventAnalyzer2
-    *process.DiMuonAnalyzer1
+    *process.DiMuonAnalyzer3
+    *process.DiMuonAnalyzer4
+#    *process.DiMuonAnalyzer5
     *process.dimuonSelection
-    *process.EventAnalyzer3
-    *process.DiMuonAnalyzer2
+    *process.MuonAnalyzer6
+    *process.DiMuonAnalyzer6
+    
+    *process.buildJetCollections
+    *process.JetAnalyzer7
+    *process.JetAnalyzer8
+    *process.jetSelection
+    *process.MuonAnalyzer9
+    *process.DiMuonAnalyzer9
+    *process.JetAnalyzer9
 )
+
+
+
+process.sameChargeAnalysis = cms.Path(
+    process.triggerFilter1
+    
+    *process.buildMuonCollections
+    *process.muonSelection
+    
+    *process.buildDimuonSCCollections
+    *process.DiMuonSCAnalyzer3
+    *process.DiMuonSCAnalyzer4
+#    *process.DiMuonSCAnalyzer5
+    *process.dimuonSCSelection
+    *process.MuonSCAnalyzer6
+    *process.DiMuonSCAnalyzer6
+    
+    *process.buildJetCollections
+    *process.JetSCAnalyzer7
+    *process.JetSCAnalyzer8
+    *process.jetSelection
+    *process.MuonSCAnalyzer9
+    *process.DiMuonSCAnalyzer9
+    *process.JetSCAnalyzer9
+)
+
+
+
