9bc4702ce5701bda049809ef9845f58768f6a464
hauk <>
2011-01-27 14:55:09 +0000

    Script to copy localTestFile from dCache to Lustre

---
+++ PhysicsAnalysis/ZmumuAnalysis/Configuration/scripts/copyLocalTestSample.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+srmcp -2 -streams_num=1 "srm://dcache-se-cms.desy.de:8443//pnfs/desy.de/cms/tier2/store/user/dammann/DYJetsToLL_TuneD6T_M-50_7TeV-madgraph-tauola/Fall10-PAT-v2/43e23e1dee19d970b0c8344e9053309f/mcpat_1_1_eXu.root" "file:////scratch/hh/current/cms/user/hauk/data/testPatTuple/mcpat_1_1_eXu.root"
