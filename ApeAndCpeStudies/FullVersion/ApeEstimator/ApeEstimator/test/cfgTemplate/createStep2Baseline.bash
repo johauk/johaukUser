8eb81ef621d93b787a53d9bb9eaa406bb3eff379
hauk <>
2011-01-29 13:31:44 +0000

    Steering scripts for automated workflow to set baseline

---
+++ ApeAndCpeStudies/FullVersion/ApeEstimator/ApeEstimator/test/cfgTemplate/createStep2Baseline.bash
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+
+
+
+
+
+
+
+#######################################################
+## Config for summary step
+
+
+cp summaryBaseline_cfg.py workingArea/summary_cfg.py
+
+
+
+
+
+
+
+
+#######################################################
+## Create final output directory
+
+
+ROOTFILEBASE="$CMSSW_BASE/src/ApeEstimator/ApeEstimator/hists/workingArea"
+
+mkdir ${ROOTFILEBASE}/../Design
+mkdir ${ROOTFILEBASE}/../Design/baseline
+
+
+
+
+
+
+
+#######################################################
+## Add root files from step1 and delete them, kepp only summed file
+
+
+
+hadd ${ROOTFILEBASE}/../Design/baseline/allData.root ${ROOTFILEBASE}/*.root
+
+rm ${ROOTFILEBASE}/*.root
+
+
+
+
+
