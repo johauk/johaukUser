69774293b22c71a8b4f7570638f92137185b33c4
hauk <>
2011-02-10 12:01:50 +0000

    Adjustments

--- PhysicsAnalysis/ZmumuAnalysis/Configuration/macros/generator_mu_etaHigh.C
+++ PhysicsAnalysis/ZmumuAnalysis/Configuration/macros/generator_mu_etaHigh.C
@@ -107,11 +107,27 @@ TLine *line1, *line2, *line3, *line4;
   //hist1->GetXaxis()->SetNoExponent();
   hist1->SetLineWidth(2);
   
-  //hist1->GetXaxis()->SetRangeUser(20.,180.);
+  hist1->GetXaxis()->SetRangeUser(-8.5,8.4);
   
-  yMax1 = hist1->GetMaximum(); yMax2 = hist2->GetMaximum();
-  yMin1 = hist1->GetMinimum(); yMin2 = hist2->GetMinimum();
-  if(yMax2>yMax1)hist1->GetYaxis()->SetRangeUser(0.,1.1*yMax2);
+  Double_t scaleFactor1(1.);
+  Double_t scaleFactor2(1.);
+  //Int_t xIntegralLow(hist1->GetXaxis()->FindBin(50.)), xIntegralHigh(hist1->GetNbinsX()+1);
+  //if(hist1->Integral(xIntegralLow,xIntegralHigh)>0.)scaleFactor1 = 100./hist1->Integral(xIntegralLow,xIntegralHigh);
+  //if(hist2->Integral(xIntegralLow,xIntegralHigh)>0.)scaleFactor2 = 100./hist2->Integral(xIntegralLow,xIntegralHigh);
+  if(hist1->Integral()>0.)scaleFactor1 = 100./hist1->GetEntries();
+  if(hist2->Integral()>0.)scaleFactor2 = 100./hist2->GetEntries();
+  Double_t maxScaleFactor(scaleFactor1>scaleFactor2 ? scaleFactor1 : scaleFactor2);
+  hist1->Scale(scaleFactor1);
+  hist1->SetYTitle(TString(hist1->GetYaxis()->GetTitle()) + "  (scaled)");
+  hist2->Scale(scaleFactor2);
+  
+  //yMax1 = hist1->GetMaximum(); yMax2 = hist2->GetMaximum();
+  //yMin1 = hist1->GetMinimum(); yMin2 = hist2->GetMinimum();   // Avoid bins out of range if range of Xaxis gets reduced (SetRange, SetRangeUser)
+  yMax1 = hist1->GetBinContent(hist1->GetMaximumBin()); yMax2 = hist2->GetBinContent(hist1->GetMaximumBin());
+  yMin1 = hist1->GetBinContent(hist1->GetMinimumBin()); yMin2 = hist2->GetBinContent(hist1->GetMinimumBin());
+  Double_t yMin(yMin1>yMin2 ? yMin2 : yMin1), yMax(yMax1>yMax2 ? yMax1 : yMax2);
+  Double_t yMinHist(0.0*yMin), yMaxHist(1.1*yMax);
+  if(yMax>yMin)hist1->GetYaxis()->SetRangeUser(yMinHist,yMaxHist);
   hist1->Draw();
 
   //hist1->SetMarkerColor(4);
@@ -140,23 +156,21 @@ TLine *line1, *line2, *line3, *line4;
   //stats2->SetLineColor(2);
   //stats2->SetTextColor(2);
   
-  Double_t yMin(hist1->GetMinimum()), yMax(hist1->GetMaximum());
-  line1 = new TLine(-2.4, yMin,-2.4, yMax);line1->SetLineStyle(2);line1->SetLineWidth(2);
-  line2 = new TLine(2.4, yMin,2.4, yMax);line2->SetLineStyle(2);line2->SetLineWidth(2);
-  //line3 = new TLine(106.,yMin,106.,yMax);line3->SetLineStyle(2);line3->SetLineWidth(2);
-  //line4 = new TLine(120.,yMin,120.,yMax);line4->SetLineStyle(2);line4->SetLineWidth(2);
+  line1 = new TLine(-2.4, yMinHist,-2.4, yMaxHist);line1->SetLineStyle(2);line1->SetLineWidth(2);
+  line2 = new TLine(2.4,  yMinHist,2.4,  yMaxHist);line2->SetLineStyle(2);line2->SetLineWidth(2);
+  //line3 = new TLine(106.,yMinHist,106.,yMaxHist);line3->SetLineStyle(2);line3->SetLineWidth(2);
+  //line4 = new TLine(120.,yMinHist,120.,yMaxHist);line4->SetLineStyle(2);line4->SetLineWidth(2);
   line1->Draw("same"); line2->Draw("same"); //line3->Draw("same"); line4->Draw("same");
 
     canvas1->Modified();
     canvas1->Update();
 
-  legend1 = new TLegend(0.71,0.77,0.91,0.92);
+  legend1 = new TLegend(0.71,0.82,0.91,0.92);
   legend1->SetFillColor(0);
-  legend1->SetFillStyle(0);
+  legend1->SetFillStyle(0); // hollow, but with border //legend1->SetFillStyle(1001); solid style, but w/o frame
+  legend1->SetBorderSize(0);
   legend1->SetTextSize(0.04);
   legend1->SetMargin(0.24);
-  legend1->SetFillStyle(1001);
-  //legend1->SetBorderSize(0);
   legend1->AddEntry(hist1,legendEntry1,"l");
   legend1->AddEntry(hist2,legendEntry2,"l");
   legend1->Draw("same");
@@ -168,6 +182,13 @@ TLine *line1, *line2, *line3, *line4;
 
     canvas1->Print(plotName + ".eps");
     canvas1->Print(plotName + ".png");
+    
+  yMinHist = (yMin<maxScaleFactor ? 0.5*maxScaleFactor : 0.5*yMin);
+  yMaxHist = (pow(yMax,1.1))/(pow(yMinHist,0.1));
+  if(yMax>yMin)hist1->SetMinimum(yMinHist);
+  if(yMax>yMin)hist1->SetMaximum(yMaxHist);
+  
+  if(line1)line1->SetY2(yMaxHist); if(line2)line2->SetY2(yMaxHist); if(line3)line3->SetY2(yMaxHist); if(line4)line4->SetY2(yMaxHist);
     
     gPad->SetLogy(1);
     canvas1->Update();
