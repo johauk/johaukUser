7c93345765bb6ee382f5c163e2647ce59bf7f9a3
hauk <>
2011-02-17 16:07:48 +0000

    Adjustments

--- PhysicsAnalysis/ZmumuAnalysis/Configuration/python/samples/testSample_cff.py
+++ PhysicsAnalysis/ZmumuAnalysis/Configuration/python/samples/testSample_cff.py
@@ -2,6 +2,7 @@
 
 ## link to files on dCache
 #from ZmumuAnalysis.Configuration.samples.dataAndFall10.samples.DYToMuMu_M_20_CT10_TuneZ2_7TeV_powheg_pythia_Fall10_START38_V12_PAT_v2_cff import source
+#from ZmumuAnalysis.Configuration.samples.dataAndFall10.samples.DYJetsToLL_M50_madgraph_F10_cff import source
 
 ## link to locally stored small test file
 from ZmumuAnalysis.Configuration.samples.localTestSample_cff import source
