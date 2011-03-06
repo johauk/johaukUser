08c4e4b54996ad1c6c44c9e10979327075e266ad
hauk <>
2011-03-06 20:03:34 +0000

    Update

--- ApeAndCpeStudies/FullVersion/ApeEstimator/ApeEstimator/test/ParticleGun/Template/jobOutputDelete.bash
+++ ApeAndCpeStudies/FullVersion/ApeEstimator/ApeEstimator/test/ParticleGun/Template/jobOutputDelete.bash
@@ -1,21 +1,23 @@
 #!/bin/bash
 
+
 # input files to submit to batch queue (comment out next line to avoid unwanted deletion)
 #INPUTBASE="raw"
+INPUTBASE="reco"
 INPUTSUFFIX=".root"
 
-# initial seed
-declare -i seedBase=0
+
 # increment counter
-declare -i counter=1
+declare -i counter=100
 
 
-while [ $counter -le 1000 ]
+while [ $counter -le 823 ]
 do
   
   theFilename="${INPUTBASE}${counter}${INPUTSUFFIX}"
   
-  rfrm /castor/cern.ch/cms/store/caf/user/hauk/mc/ParticleGunPion/RAW/$theFilename
+  #rfrm /castor/cern.ch/cms/store/caf/user/hauk/mc/ParticleGunPion/RAW/$theFilename
+  rfrm /castor/cern.ch/cms/store/caf/user/hauk/data/Mu/Run2010A_Dec22ReReco/$theFilename
   
   counter=$counter+1
 
