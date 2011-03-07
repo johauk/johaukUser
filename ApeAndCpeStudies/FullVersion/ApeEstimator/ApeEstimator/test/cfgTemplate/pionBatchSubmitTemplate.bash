3fe8d58a7ed0b8631733a54aca7913607eb334ab
hauk <>
2011-03-07 11:39:56 +0000

    Renaming of files

---
+++ ApeAndCpeStudies/FullVersion/ApeEstimator/ApeEstimator/test/cfgTemplate/pionBatchSubmitTemplate.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export PYTHONHOME=/afs/cern.ch/cms/sw/slc5_ia32_gcc434/external/python/2.6.4-cms
+
+export STAGE_SVCCLASS=cmscafuser
+
+
+rfcp /castor/cern.ch/cms/store/caf/user/hauk/mc/ParticleGunPion/RECO/reco_THE_NUMBER_.root reco.root
+
+
+cmsRun $CMSSW_BASE/src/ApeEstimator/ApeEstimator/test/batch/workingArea/_THE_NAME___THE_NUMBER__cfg.py
