685e370ecf3971cc7a6a1bc1c3509797d7eb4b20
hauk <>
2011-03-10 14:34:29 +0000

    Private skim for APE calculation

---
+++ ApeAndCpeStudies/FullVersion/ApeEstimator/ApeEstimator/python/samples/Data_TkAlMuonIsolated_Run2010A_Dec22ReReco_ApeSkim_cff.py
@@ -0,0 +1,24 @@
+import FWCore.ParameterSet.Config as cms
+
+maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+readFiles = cms.untracked.vstring()
+secFiles = cms.untracked.vstring() 
+source = cms.Source ("PoolSource",fileNames = readFiles, secondaryFileNames = secFiles)
+
+from ApeEstimator.ApeEstimator.samples.GoodRunList_cff import LumisToProcess
+source.lumisToProcess = LumisToProcess
+
+readFiles.extend( [
+
+'rfio:///?svcClass=cmscafuser&path=/castor/cern.ch/cms/store/caf/user/hauk/data/Mu/Run2010A_Dec22ReReco/apeSkim1.root',
+'rfio:///?svcClass=cmscafuser&path=/castor/cern.ch/cms/store/caf/user/hauk/data/Mu/Run2010A_Dec22ReReco/apeSkim2.root',
+'rfio:///?svcClass=cmscafuser&path=/castor/cern.ch/cms/store/caf/user/hauk/data/Mu/Run2010A_Dec22ReReco/apeSkim3.root',
+'rfio:///?svcClass=cmscafuser&path=/castor/cern.ch/cms/store/caf/user/hauk/data/Mu/Run2010A_Dec22ReReco/apeSkim4.root',
+
+
+] );
+
+
+secFiles.extend( [
+               ] )
+
