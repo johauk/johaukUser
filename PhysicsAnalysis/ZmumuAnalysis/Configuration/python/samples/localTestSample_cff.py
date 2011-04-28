e25d58654bf41cfdc99df25fc19874de6e56fc0a
hauk <>
2011-04-28 10:04:45 +0000

    Update

--- PhysicsAnalysis/ZmumuAnalysis/Configuration/python/samples/localTestSample_cff.py
+++ PhysicsAnalysis/ZmumuAnalysis/Configuration/python/samples/localTestSample_cff.py
@@ -6,7 +6,8 @@ source = cms.Source ("PoolSource",fileNames = readFiles )
 readFiles.extend( [
 
 
-'file:/scratch/hh/current/cms/user/hauk/data/testPatTuple/mcpat_1_1_eXu_madgraph_D6T.root',
+#'file:/scratch/hh/current/cms/user/hauk/data/testPatTuple/mcpat_1_1_eXu_madgraph_D6T.root',
+'file:/scratch/hh/current/cms/user/hauk/data/testPatTuple/mcpat_1_3_bfP_madgraph_Z2.root',
 
 
 ] );
--- PhysicsAnalysis/ZmumuAnalysis/Configuration/scripts/copyLocalTestSample.sh
+++ PhysicsAnalysis/ZmumuAnalysis/Configuration/scripts/copyLocalTestSample.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-srmcp -2 -streams_num=1 "srm://dcache-se-cms.desy.de:8443//pnfs/desy.de/cms/tier2/store/user/dammann/DYJetsToLL_TuneD6T_M-50_7TeV-madgraph-tauola/Fall10-PAT-v2/43e23e1dee19d970b0c8344e9053309f/mcpat_1_1_eXu.root" "file:////scratch/hh/current/cms/user/hauk/data/testPatTuple/mcpat_1_1_eXu.root"
+#srmcp -2 -streams_num=1 "srm://dcache-se-cms.desy.de:8443//pnfs/desy.de/cms/tier2/store/user/dammann/DYJetsToLL_TuneD6T_M-50_7TeV-madgraph-tauola/Fall10-PAT-v2/43e23e1dee19d970b0c8344e9053309f/mcpat_1_1_eXu.root" "file:////scratch/hh/current/cms/user/hauk/data/testPatTuple/mcpat_1_1_eXu_madgraph_D6T.root"
+
+srmcp -2 -streams_num=1 "srm://dcache-se-cms.desy.de:8443//pnfs/desy.de/cms/tier2/store/user/wbehrenh/ZbbToLL_M-40_PtB1-15_TuneZ2_7TeV-madgraph-pythia6/Fall10_PAT/43e23e1dee19d970b0c8344e9053309f/mcpat_1_3_bfP.root" "file:////scratch/hh/current/cms/user/hauk/data/testPatTuple/mcpat_1_3_bfP_madgraph_Z2.root"
+
+
+
+
+
