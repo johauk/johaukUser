a53936d91baa49201825de4e5a47c62f0a41b758
hauk <>
2011-04-28 12:37:40 +0000

    Cleanup

--- PhysicsAnalysis/ZmumuAnalysis/Analyzer/test/metAnalyzer_cfg.py
+++ PhysicsAnalysis/ZmumuAnalysis/Analyzer/test/metAnalyzer_cfg.py
@@ -15,10 +15,6 @@ process.MessageLogger.cerr.FwkReport.reportEvery = 1000 ## really show only ever
 
 process.options = cms.untracked.PSet(
     wantSummary = cms.untracked.bool(True),
-    #fileMode  =  cms.untracked.string('FULLMERGE'), # any file order (default): caches all lumi/run products (memory!)
-    #fileMode  =  cms.untracked.string('MERGE'), # needs files sorted in run and within run in lumi sections (hard to achieve)
-    #fileMode  =  cms.untracked.string('FULLLUMIMERGE'), # needs files sorted in run, caches lumi
-    fileMode  =  cms.untracked.string('NOMERGE'), #  no ordering needed, but calls endRun/beginRun etc. at file boundaries
 )
 
 
