74e78d6c5a27c06f2cd8fa3487add5c28cb44520
hauk <>
2011-05-06 11:35:38 +0000

    Test file

---
+++ PhysicsAnalysis/ZmumuAnalysis/Producer/test/bestZVertexCleaner_cfg.py
@@ -0,0 +1,64 @@
+import os
+
+import FWCore.ParameterSet.Config as cms
+
+
+process = cms.Process("BestZVertexProducer")
+
+
+
+## Message logger
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000 ## really show only every 1000th
+
+
+
+process.options = cms.untracked.PSet(
+    wantSummary = cms.untracked.bool(True),
+)
+
+
+
+## Input Source
+process.load("ZmumuAnalysis.Configuration.samples.testSample_cff")
+
+
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(101) )
+
+
+
+## di-muon candidate producer
+from ElectroWeakAnalysis.Skimming.dimuons_cfi import *
+process.selectedDimuons = dimuons.clone(
+    checkCharge = False,
+    cut = #'daughter(0).isGlobalMuon = 1' +'&'+
+	  #'daughter(1).isGlobalMuon = 1' +'&'+
+	  'mass > 0',
+    decay = 'selectedPatMuons@+ selectedPatMuons@-',
+)
+
+
+
+## Analyzer under test
+process.load("ZmumuAnalysis.Producer.BestZVertexCleaner_cfi")
+process.BestZVertexCleaner1 = process.BestZVertexCleaner.clone(
+    #product = "dimuon",
+    product = "vertex",
+    #vertexSource = 'offlinePrimaryVertices',
+    #dimuonSource = 'selectedDimuons',
+    #deltaZMax = 0.1,
+)
+
+
+
+## Path
+process.p = cms.Path(
+    process.selectedDimuons*
+    process.BestZVertexCleaner1
+)
+
+
+
+
+
