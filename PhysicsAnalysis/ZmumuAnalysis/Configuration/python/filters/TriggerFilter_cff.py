684ad831d5aadf32c26bda1548f6b5e69b589492
hauk <>
2011-05-12 13:44:31 +0000

    New selection for DoubleMu6 added

--- PhysicsAnalysis/ZmumuAnalysis/Configuration/python/filters/TriggerFilter_cff.py
+++ PhysicsAnalysis/ZmumuAnalysis/Configuration/python/filters/TriggerFilter_cff.py
@@ -4,16 +4,20 @@ import HLTrigger.HLTfilters.hltHighLevel_cfi
 
 
 
-TriggerFilterMu9 =  HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone(
-    HLTPaths = ["HLT_Mu9"],
+
+
+
+
+TriggerFilterMu9_11_15 = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone(
+    #TriggerResultsTag = cms.InputTag('TriggerResults','','HLT'),   # default
+    HLTPaths = ["HLT_Mu9","HLT_Mu11","HLT_Mu15","HLT_Mu15_v*"],
+    throw = False,   # do not throw exception on events where not all listed triggers are in...
 )
 
 
 
-TriggerFilterMu9_11_15 =  HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone(
-    #TriggerResultsTag = cms.InputTag('TriggerResults','','HLT'),   # default
-    HLTPaths = ["HLT_Mu9","HLT_Mu11","HLT_Mu15","HLT_Mu15_v*"],
-    throw = False,   # do not throw exception on events where not all listed triggers are in...
+TriggerFilterMu9 = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone(
+    HLTPaths = ["HLT_Mu9"],
 )
 
 
@@ -25,10 +29,21 @@ TriggerFilterMu9_11_15_Redigi = TriggerFilterMu9_11_15.clone(
 
 
 # different trigger versions (bla_v*) are only in data, does not select events on MC at present (use there pure version w/o suffix)
-TriggerFilterMu15 =  HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone(
+TriggerFilterMu15 = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone(
     HLTPaths = ["HLT_Mu15","HLT_Mu15_v*"],
     throw = False,
 )
 
 
+
+TriggerFilterDoubleMu6 = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone(
+    HLTPaths = ["HLT_DoubleMu6_v*"],
+    throw = False,   # do not throw exception on events where not all listed triggers are in...
+)
+
+
+##
+## Set the recent default here
+##
+TriggerFilter = TriggerFilterDoubleMu6.clone()
 
