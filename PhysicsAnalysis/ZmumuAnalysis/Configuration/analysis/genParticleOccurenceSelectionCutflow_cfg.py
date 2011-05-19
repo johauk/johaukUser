14bc87772c74583a2f382cd615a43cdb94c8a74c
hauk <>
2011-05-19 15:00:00 +0000

    Updates

--- PhysicsAnalysis/ZmumuAnalysis/Configuration/analysis/genParticleOccurenceSelectionCutflow_cfg.py
+++ PhysicsAnalysis/ZmumuAnalysis/Configuration/analysis/genParticleOccurenceSelectionCutflow_cfg.py
@@ -9,7 +9,7 @@ process = cms.Process("GenParticleOccurenceCutflow")
 ## add message logger
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.MessageLogger.cerr.threshold = 'INFO'
-process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+process.MessageLogger.cerr.FwkReport.reportEvery = 1
 
 
 
@@ -21,18 +21,18 @@ process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 #process.load("ZmumuAnalysis.Configuration.samples.dataAndFall10.samples.DYToTauTau_M10To20_pythia_F10_cff")
 #process.load("ZmumuAnalysis.Configuration.samples.dataAndFall10.samples.DYToTauTau_M20_pythia_F10_cff")
 # To use for Zmumu (Powheg)
-process.load("ZmumuAnalysis.Configuration.samples.dataAndFall10.samples.DYToMuMu_M_20_CT10_TuneZ2_7TeV_powheg_pythia_Fall10_START38_V12_PAT_v2_cff")
+#process.load("ZmumuAnalysis.Configuration.samples.dataAndFall10.samples.DYToMuMu_M_20_CT10_TuneZ2_7TeV_powheg_pythia_Fall10_START38_V12_PAT_v2_cff")
 # To use for Ztautau
 #process.load("ZmumuAnalysis.Configuration.samples.dataAndFall10.samples.DYJetsToLL_M50_D6T_madgraph_F10_cff")
 #process.load("ZmumuAnalysis.Configuration.samples.dataAndFall10.samples.WZTo3LNu_pythia_F10_cff")
 #process.load("ZmumuAnalysis.Configuration.samples.dataAndFall10.samples.ZZToAnything_pythia_F10_cff")
-#process.load("ZmumuAnalysis.Configuration.samples.testSample_cff")
+process.load("ZmumuAnalysis.Configuration.samples.testSample_cff")
 
 
 
 ## define maximal number of events to loop over
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(1001)
+    input = cms.untracked.int32(5)
 )
 
 
@@ -108,6 +108,37 @@ process.GeneratorZmumuAnalyzerB4 =  process.GeneratorZmumuAnalyzer.clone()
 
 
 
+process.goodB = cms.EDFilter("GenParticleSelector",
+    filter = cms.bool(True),
+    src = cms.InputTag("genParticles"),
+    cut = cms.string('abs(pdgId)=5 & (status=2 || status=3) & pt>10.'),
+)
+
+
+process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
+process.printTree = cms.EDAnalyzer("ParticleTreeDrawer",
+    src = cms.InputTag("genParticles"),
+    #printP4 = cms.untracked.bool(True),
+    #printPtEtaPhi = cms.untracked.bool(True),
+    #printVertex = cms.untracked.bool(True),
+    printStatus = cms.untracked.bool(True),
+    #printIndex = cms.untracked.bool(True),
+    #status = cms.untracked.vint32(1,2,3),
+    status = cms.untracked.vint32(3),
+)
+
+process.printDecay = cms.EDAnalyzer("ParticleDecayDrawer",
+    src = cms.InputTag("genParticles"),
+    status = cms.untracked.vint32(3),
+    #maxEventsToPrint = cms.untracked.int32(3),
+    #printP4 = cms.untracked.bool(True),
+    #printPtEtaPhi = cms.untracked.bool(True),
+    #printVertex = cms.untracked.bool(True),
+)
+
+
+
+
 #******************************************************************************************
 #   Analysis Paths
 #******************************************************************************************
@@ -115,29 +146,37 @@ process.GeneratorZmumuAnalyzerB4 =  process.GeneratorZmumuAnalyzer.clone()
 
 
 process.UdsFlavourSelection = cms.Path(
-    process.EventCounter0
-    *process.GeneratorZmumuUdsFilter1
-    *process.GenParticleOccurenceFilter1
-    *process.GenParticleOccurenceAnalyzerUds1
-    *process.GeneratorZmumuAnalyzerUds1
+    process.EventCounter0*
+    process.printTree*
+    #process.printDecay*
+    process.goodB*
+    process.GeneratorZmumuUdsFilter1*
+    #process.goodB*
+    process.GenParticleOccurenceFilter1*
+    process.GenParticleOccurenceAnalyzerUds1*
+    process.GeneratorZmumuAnalyzerUds1
 )
 
 
 
 process.CFlavourSelection = cms.Path(
-    process.GeneratorZmumuCFilter1
-    *process.GenParticleOccurenceFilter1
-    *process.GenParticleOccurenceAnalyzerC1
-    *process.GeneratorZmumuAnalyzerC1
+    process.goodB*
+    process.GeneratorZmumuCFilter1*
+    #process.goodB*
+    process.GenParticleOccurenceFilter1*
+    process.GenParticleOccurenceAnalyzerC1*
+    process.GeneratorZmumuAnalyzerC1
 )
 
 
 
 process.BFlavourSelection = cms.Path(
-    process.GeneratorZmumuBFilter1
-    *process.GenParticleOccurenceFilter1
-    *process.GenParticleOccurenceAnalyzerB1
-    *process.GeneratorZmumuAnalyzerB1
+    process.goodB*
+    process.GeneratorZmumuBFilter1*
+    #process.goodB*
+    process.GenParticleOccurenceFilter1*
+    process.GenParticleOccurenceAnalyzerB1*
+    process.GeneratorZmumuAnalyzerB1
 )
 
 
