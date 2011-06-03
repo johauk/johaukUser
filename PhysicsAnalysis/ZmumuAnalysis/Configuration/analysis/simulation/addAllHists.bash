5aa1b082a79b6f2ff3e71de9b5fdb9a6d2400c78
hauk <>
2011-06-03 00:25:09 +0000

    Separate signal via b quarks

--- PhysicsAnalysis/ZmumuAnalysis/Configuration/analysis/simulation/addAllHists.bash
+++ PhysicsAnalysis/ZmumuAnalysis/Configuration/analysis/simulation/addAllHists.bash
@@ -4,10 +4,15 @@
 DIRBASE="$CMSSW_BASE/src/ZmumuAnalysis/Configuration"
 
 
-hadd $DIRBASE/hists/simulation/generatorTopZmumu.root $DIRBASE/analysis/simulation/naf_generatorTopZmumu_cfg/*.root
-sumTriggerReports2.pl $DIRBASE/analysis/simulation/naf_generatorTopZmumu_cfg/out*.txt > $DIRBASE/hists/simulation/generatorTopZmumu.txt
+#hadd $DIRBASE/hists/simulation/generatorTopZmumu.root $DIRBASE/analysis/simulation/naf_generatorTopZmumu_cfg/*.root
+#sumTriggerReports2.pl $DIRBASE/analysis/simulation/naf_generatorTopZmumu_cfg/out*.txt > $DIRBASE/hists/simulation/generatorTopZmumu.txt
 
 
-hadd $DIRBASE/hists/simulation/generatorTopZmumu_powheg.root $DIRBASE/analysis/simulation/naf_generatorTopZmumu_powheg_cfg/*.root
-sumTriggerReports2.pl $DIRBASE/analysis/simulation/naf_generatorTopZmumu_powheg_cfg/out*.txt > $DIRBASE/hists/simulation/generatorTopZmumu_powheg.txt
+#hadd $DIRBASE/hists/simulation/generatorTopZmumu_powheg.root $DIRBASE/analysis/simulation/naf_generatorTopZmumu_powheg_cfg/*.root
+#sumTriggerReports2.pl $DIRBASE/analysis/simulation/naf_generatorTopZmumu_powheg_cfg/out*.txt > $DIRBASE/hists/simulation/generatorTopZmumu_powheg.txt
 
+
+
+
+hadd $DIRBASE/hists/simulation/genParticle.root $DIRBASE/analysis/simulation/naf_genParticle_cfg/*.root
+sumTriggerReports2.pl $DIRBASE/analysis/simulation/naf_genParticle_cfg/out*.txt > $DIRBASE/hists/simulation/genParticle.txt
