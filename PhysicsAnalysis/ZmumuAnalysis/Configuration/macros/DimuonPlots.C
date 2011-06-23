d9805997ad81538a803a13b112055ac61042cf57
hauk <>
2011-06-23 09:54:58 +0000

    Adjustment

--- PhysicsAnalysis/ZmumuAnalysis/Configuration/macros/DimuonPlots.C
+++ PhysicsAnalysis/ZmumuAnalysis/Configuration/macros/DimuonPlots.C
@@ -269,6 +269,7 @@ void DimuonPlots(TString pluginSuffix = ""){
   canvas1->Print(outpath->Copy().Append(*plotName1).Append(pluginSuffix).Append(*outform));
   
   gPad->SetLogy(1);
+  //stack1->SetMinimum(0.01);
   canvas1->Update();
   canvas1->Print(outpath->Copy().Append(*plotName1).Append(pluginSuffix).Append("_log").Append(*outform));
       
