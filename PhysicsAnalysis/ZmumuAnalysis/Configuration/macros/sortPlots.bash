a787f450d05df53e2c20d6c1a47fee7e72657e28
hauk <>
2011-06-23 10:02:56 +0000

    New macro and script for automation of plotting and sorting of plots

---
+++ PhysicsAnalysis/ZmumuAnalysis/Configuration/macros/sortPlots.bash
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+
+STEP="2b"
+STEPDIR="plots/Step${STEP}/"; mkdir $STEPDIR; for file in plots/*Step${STEP}*.png; do mv $file $STEPDIR; done
+
+STEP="2"
+STEPDIR="plots/Step${STEP}/"; mkdir $STEPDIR; for file in plots/*Step${STEP}*.png; do mv $file $STEPDIR; done
+
+STEP="3d"
+STEPDIR="plots/Step${STEP}/"; mkdir $STEPDIR; for file in plots/*Step${STEP}*.png; do mv $file $STEPDIR; done
+
+STEP="3e"
+STEPDIR="plots/Step${STEP}/"; mkdir $STEPDIR; for file in plots/*Step${STEP}*.png; do mv $file $STEPDIR; done
+
+STEP="3f"
+STEPDIR="plots/Step${STEP}/"; mkdir $STEPDIR; for file in plots/*Step${STEP}*.png; do mv $file $STEPDIR; done
+
+STEP="3"
+STEPDIR="plots/Step${STEP}/"; mkdir $STEPDIR; for file in plots/*Step${STEP}*.png; do mv $file $STEPDIR; done
+
+STEP="4a"
+STEPDIR="plots/Step${STEP}/"; mkdir $STEPDIR; for file in plots/*Step${STEP}*.png; do mv $file $STEPDIR; done
+
+STEP="4b"
+STEPDIR="plots/Step${STEP}/"; mkdir $STEPDIR; for file in plots/*Step${STEP}*.png; do mv $file $STEPDIR; done
+
+STEP="4c"
+STEPDIR="plots/Step${STEP}/"; mkdir $STEPDIR; for file in plots/*Step${STEP}*.png; do mv $file $STEPDIR; done
+
+STEP="4d"
+STEPDIR="plots/Step${STEP}/"; mkdir $STEPDIR; for file in plots/*Step${STEP}*.png; do mv $file $STEPDIR; done
+
+STEP="4"
+STEPDIR="plots/Step${STEP}/"; mkdir $STEPDIR; for file in plots/*Step${STEP}*.png; do mv $file $STEPDIR; done
+
+STEP="5"
+STEPDIR="plots/Step${STEP}/"; mkdir $STEPDIR; for file in plots/*Step${STEP}*.png; do mv $file $STEPDIR; done
+
+STEP="6a"
+STEPDIR="plots/Step${STEP}/"; mkdir $STEPDIR; for file in plots/*Step${STEP}*.png; do mv $file $STEPDIR; done
+
+STEP="6"
+STEPDIR="plots/Step${STEP}/"; mkdir $STEPDIR; for file in plots/*Step${STEP}*.png; do mv $file $STEPDIR; done
+
+
+
+
+
+
+
+STEP="ZVeto3f"
+STEPDIR="plots/Step${STEP}/"; mkdir $STEPDIR; for file in plots/*Step${STEP}*.png; do mv $file $STEPDIR; done
+
+STEP="ZVeto3"
+STEPDIR="plots/Step${STEP}/"; mkdir $STEPDIR; for file in plots/*Step${STEP}*.png; do mv $file $STEPDIR; done
+
+STEP="ZVeto4d"
+STEPDIR="plots/Step${STEP}/"; mkdir $STEPDIR; for file in plots/*Step${STEP}*.png; do mv $file $STEPDIR; done
+
+STEP="ZVeto4"
+STEPDIR="plots/Step${STEP}/"; mkdir $STEPDIR; for file in plots/*Step${STEP}*.png; do mv $file $STEPDIR; done
+
+STEP="ZVeto5"
+STEPDIR="plots/Step${STEP}/"; mkdir $STEPDIR; for file in plots/*Step${STEP}*.png; do mv $file $STEPDIR; done
+
+STEP="ZVeto6a"
+STEPDIR="plots/Step${STEP}/"; mkdir $STEPDIR; for file in plots/*Step${STEP}*.png; do mv $file $STEPDIR; done
+
+STEP="ZVeto6"
+STEPDIR="plots/Step${STEP}/"; mkdir $STEPDIR; for file in plots/*Step${STEP}*.png; do mv $file $STEPDIR; done
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
