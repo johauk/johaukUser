8f418ddc1b5041a1815b64b1ab41bab2a34c97ba
hauk <>
2011-07-25 23:01:09 +0000

    Update

--- PhysicsAnalysis/ZmumuAnalysis/Configuration/scripts/initialise.bash
+++ PhysicsAnalysis/ZmumuAnalysis/Configuration/scripts/initialise.bash
@@ -9,6 +9,8 @@ mkdir $DIRBASE/Configuration/hists/data/
 mkdir $DIRBASE/Configuration/hists/mc/
 mkdir $DIRBASE/Configuration/hists/simulation/
 
+mkdir $DIRBASE/Configuration/macros/plots/
+
 
 
 mkdir $DIRBASE/Analyzer/hists/
