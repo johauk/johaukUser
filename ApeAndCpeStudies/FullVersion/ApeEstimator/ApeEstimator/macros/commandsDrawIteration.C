076a4f2568fd6569f6410139b25a3e8ddcdab2b1
hauk <>
2011-08-01 16:55:32 +0000

    Default setup with all subdetectors

---
+++ ApeAndCpeStudies/FullVersion/ApeEstimator/ApeEstimator/macros/commandsDrawIteration.C
@@ -0,0 +1,20 @@
+{
+
+
+gROOT->ProcessLine(".L drawIteration.C+");
+
+DrawIteration a1(7);
+//a1.drawIteration();
+a1.drawIteration(1,6);
+a1.drawIteration(7,14);
+a1.drawIteration(15,26);
+a1.drawIteration(27,38);
+a1.drawIteration(39,48);
+a1.drawIteration(49,68);
+
+
+
+//gROOT->ProcessLine(".q");
+
+
+}
