51a4bf8af8adf38372fa988e04f355e30ba3ccc5
hauk <>
2011-08-07 23:17:36 +0000

    Setup for new releases

---
+++ ApeAndCpeStudies/FullVersion/ApeEstimator/ApeEstimator/scripts/initialise.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DIRBASE="$CMSSW_BASE/src/ApeEstimator/ApeEstimator"
+
+
+
+mkdir $DIRBASE/hists/
+mkdir $DIRBASE/hists/workingArea/
+mkdir $DIRBASE/test/batch/workingArea/
+mkdir $DIRBASE/test/cfgTemplateData/workingArea/
+mkdir $DIRBASE/test/cfgTemplate/workingArea/
+mkdir $DIRBASE/test/cfgTemplateMc/workingArea/
+
+
+
+
+
+
