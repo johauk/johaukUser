73a5167b918bd79c79897106460c86f04a8f469a
hauk <>
2011-08-14 20:25:30 +0000

    More generic

--- ApeAndCpeStudies/FullVersion/ApeEstimator/ApeEstimator/scripts/initialise.bash
+++ ApeAndCpeStudies/FullVersion/ApeEstimator/ApeEstimator/scripts/initialise.bash
@@ -7,27 +7,25 @@ DIRBASE="$CMSSW_BASE/src/ApeEstimator/ApeEstimator"
 mkdir $DIRBASE/hists/
 mkdir $DIRBASE/hists/workingArea/
 mkdir $DIRBASE/test/batch/workingArea/
-mkdir $DIRBASE/test/cfgTemplate/workingArea/
 mkdir $DIRBASE/test/cfgTemplateMc/workingArea/
 mkdir $DIRBASE/test/cfgTemplateData/workingArea/
+mkdir $DIRBASE/test/cfgTemplateParticleGun/workingArea/
 
 
 
-cp $DIRBASE/test/cfgTemplate/batchSubmitTemplate.bash $DIRBASE/test/cfgTemplateMc/batchSubmitTemplate.bash
-cp $DIRBASE/test/cfgTemplate/createStep2Baseline.bash $DIRBASE/test/cfgTemplateMc/createStep2Baseline.bash
+cp $DIRBASE/test/cfgTemplate/createStep2.bash $DIRBASE/test/cfgTemplateMc/createStep2.bash
 cp $DIRBASE/test/cfgTemplate/startStep1.bash $DIRBASE/test/cfgTemplateMc/startStep1.bash
 cp $DIRBASE/test/cfgTemplate/startStep2.bash $DIRBASE/test/cfgTemplateMc/startStep2.bash
-cp $DIRBASE/test/cfgTemplate/summaryBaseline_cfg.py $DIRBASE/test/cfgTemplateMc/summaryBaseline_cfg.py
 
 
-cp $DIRBASE/test/cfgTemplate/apeLocalSettingTemplate_cfg.py $DIRBASE/test/cfgTemplateData/apeLocalSettingTemplate_cfg.py
-cp $DIRBASE/test/cfgTemplate/batchSubmitTemplate.bash $DIRBASE/test/cfgTemplateData/batchSubmitTemplate.bash
-cp $DIRBASE/test/cfgTemplate/createStep2IterN.bash $DIRBASE/test/cfgTemplateData/createStep2IterN.bash
+cp $DIRBASE/test/cfgTemplate/createStep2.bash $DIRBASE/test/cfgTemplateData/createStep2.bash
 cp $DIRBASE/test/cfgTemplate/startStep1.bash $DIRBASE/test/cfgTemplateData/startStep1.bash
 cp $DIRBASE/test/cfgTemplate/startStep2.bash $DIRBASE/test/cfgTemplateData/startStep2.bash
-cp $DIRBASE/test/cfgTemplate/summaryTemplate_cfg.py $DIRBASE/test/cfgTemplateData/summaryTemplate_cfg.py
 
 
+cp $DIRBASE/test/cfgTemplate/createStep2.bash $DIRBASE/test/cfgTemplateParticleGun/createStep2.bash
+cp $DIRBASE/test/cfgTemplate/startStep1.bash $DIRBASE/test/cfgTemplateParticleGun/startStep1.bash
+cp $DIRBASE/test/cfgTemplate/startStep2.bash $DIRBASE/test/cfgTemplateParticleGun/startStep2.bash
 
 
 
---
+++ ApeAndCpeStudies/FullVersion/ApeEstimator/ApeEstimator/test/cfgTemplateParticleGun/createStep1.bash
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+
+
+if [ ! $# -ge 1 ]; then
+  echo "Usage: $0 iterationNumber"
+  echo "Usage: $0 iterationNumber lastIteration"
+  exit 1
+fi
+
+export iterationNumber="$1"
+export lastIteration="False"
+if [ $# == 2 ]; then
+  lastIteration="$2";
+  if [[ ! "$lastIteration" == False ]] && [[ ! "$lastIteration" == True ]] ; then
+    echo "Invalid argument for lastIteration: $lastIteration"
+    exit 2
+  fi
+fi
+
+echo "Iteration number: $1"
+echo "LastIteration: ${lastIteration}"
+echo
+
+
+
+
+
+## Alignment
+export alignmentRcd
+alignmentRcd="design"
+#alignmentRcd="misalTob20"
+echo "Alignment Record: $alignmentRcd"
+echo
+
+
+
+## Script to create submit scripts for specific dataset
+createStep1="${CMSSW_BASE}/src/ApeEstimator/ApeEstimator/test/cfgTemplate/writeSubmitScript.sh"
+
+## identification name of dataset
+#datasetName="ParticleGunMuon"
+#datasetName="ParticleGunPion"
+#datasetName="data"
+export datasetName
+## number of input files
+export nFiles
+
+datasetName="ParticleGunMuon"
+#datasetName="ParticleGunPion"
+nFiles=10
+bash $createStep1 $datasetName $nFiles $iterationNumber $lastIteration $alignmentRcd
+
+datasetName="ParticleGunAntiMuon"
+#datasetName="ParticleGunAntiPion"
+nFiles=10
+bash $createStep1 $datasetName $nFiles $iterationNumber $lastIteration $alignmentRcd
+
+
