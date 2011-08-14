8e0ca42b0247e33f00def9f74b5aafcd0ab091ec
hauk <>
2011-08-14 00:00:57 +0000

    Steerable via VarParsing

--- ApeAndCpeStudies/FullVersion/ApeEstimator/ApeEstimator/test/testApeestimatorSummary_cfg.py
+++ ApeAndCpeStudies/FullVersion/ApeEstimator/ApeEstimator/test/testApeestimatorSummary_cfg.py
@@ -3,7 +3,36 @@ import os
 import FWCore.ParameterSet.Config as cms
 
 
-process = cms.Process("Demo")
+
+
+
+##
+## Setup command line options
+##
+import FWCore.ParameterSet.VarParsing as VarParsing
+import sys
+options = VarParsing.VarParsing ('standard')
+options.register('sample', 'data', VarParsing.VarParsing.multiplicity.singleton, VarParsing.VarParsing.varType.string, "Input sample")
+options.register('isTest', True, VarParsing.VarParsing.multiplicity.singleton, VarParsing.VarParsing.varType.bool, "Test run")
+
+# get and parse the command line arguments
+if( hasattr(sys, "argv") ):
+    for args in sys.argv :
+        arg = args.split(',')
+        for val in arg:
+            val = val.split('=')
+            if(len(val)==2):
+                setattr(options,val[0], val[1])
+
+print "Input sample: ", options.sample
+print "Test run: ", options.isTest
+
+
+
+##
+## Process definition
+##
+process = cms.Process("ApeEstimatorSummary")
 
 
 
@@ -26,13 +55,45 @@ process.MessageLogger.cerr.FwkReport.reportEvery = 1000 ## really show only ever
 
 
 
+##
+## Process options
+##
 process.options = cms.untracked.PSet(
     wantSummary = cms.untracked.bool(True),
 )
 
 
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1) )
+##
+## Input sample definition
+##
+isData = False
+isQcd = isWlnu = isZmumu = isZtautau = False
+isMc = False
+isParticleGunMu = isParticleGunPi = False
+if options.sample == 'data':
+    isData = True
+elif options.sample == 'qcd':
+    isQcd = True
+    isMc = True
+elif options.sample == 'wlnu':
+    isWlnu = True
+    isMc = True
+elif options.sample == 'zmumu':
+    isZmumu = True
+    isMc = True
+elif options.sample == 'ztautau':
+    isZtautau = True
+    isMc = True
+elif options.sample == 'particleGunMu':
+    isParticleGunMu = True
+    isMc = True
+elif options.sample == 'particleGunPi':
+    isParticleGunPi = True
+    isMc = True
+else:
+    print 'ERROR --- incorrect data sammple: ', options.sample
+    exit(8888)
 
 
 
@@ -44,26 +105,30 @@ process.source = cms.Source("EmptySource")
 
 
 ##
+## Number of Events
+##
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1) )
+
+
+
+##
 ## ApeEstimatorSummary
 ##
 from ApeEstimator.ApeEstimator.ApeEstimatorSummary_cff import *
-#SAMPLE="particleGun"
-SAMPLE="ideal"
-#SAMPLE="data"
 process.ApeEstimatorSummary1 = ApeEstimatorSummaryBaseline.clone(
-    InputFile = os.environ['CMSSW_BASE'] + '/src/ApeEstimator/ApeEstimator/hists/test_' + SAMPLE + '.root',
-    ResultsFile = os.environ['CMSSW_BASE'] + '/src/ApeEstimator/ApeEstimator/hists/test_' + SAMPLE + '_resultsFile1.root',
-    BaselineFile = os.environ['CMSSW_BASE'] + '/src/ApeEstimator/ApeEstimator/hists/test_' + SAMPLE + '_baselineApe.root',
+    InputFile = os.environ['CMSSW_BASE'] + '/src/ApeEstimator/ApeEstimator/hists/test_' + options.sample + '.root',
+    ResultsFile = os.environ['CMSSW_BASE'] + '/src/ApeEstimator/ApeEstimator/hists/test_' + options.sample + '_resultsFile1.root',
+    BaselineFile = os.environ['CMSSW_BASE'] + '/src/ApeEstimator/ApeEstimator/hists/test_' + options.sample + '_baselineApe.root',
 )
 process.ApeEstimatorSummary2 = ApeEstimatorSummaryIter.clone(
     apeWeight = "entries",
     correctionScaling = 0.6,
     smoothIteration = False,
-    InputFile = os.environ['CMSSW_BASE'] + '/src/ApeEstimator/ApeEstimator/hists/test_' + SAMPLE + '.root',
-    ResultsFile = os.environ['CMSSW_BASE'] + '/src/ApeEstimator/ApeEstimator/hists/test_' + SAMPLE + '_resultsFile2.root',
-    BaselineFile = os.environ['CMSSW_BASE'] + '/src/ApeEstimator/ApeEstimator/hists/test_' + SAMPLE + '_baselineApe.root',
-    IterationFile = os.environ['CMSSW_BASE'] + '/src/ApeEstimator/ApeEstimator/hists/test_' + SAMPLE + '_iterationApe2.root',
-    ApeOutputFile = os.environ['CMSSW_BASE'] + '/src/ApeEstimator/ApeEstimator/hists/test_' + SAMPLE + '_apeOutput2.txt',
+    InputFile = os.environ['CMSSW_BASE'] + '/src/ApeEstimator/ApeEstimator/hists/test_' + options.sample + '.root',
+    ResultsFile = os.environ['CMSSW_BASE'] + '/src/ApeEstimator/ApeEstimator/hists/test_' + options.sample + '_resultsFile2.root',
+    BaselineFile = os.environ['CMSSW_BASE'] + '/src/ApeEstimator/ApeEstimator/hists/test_' + options.sample + '_baselineApe.root',
+    IterationFile = os.environ['CMSSW_BASE'] + '/src/ApeEstimator/ApeEstimator/hists/test_' + options.sample + '_iterationApe2.root',
+    ApeOutputFile = os.environ['CMSSW_BASE'] + '/src/ApeEstimator/ApeEstimator/hists/test_' + options.sample + '_apeOutput2.txt',
 )
 process.ApeEstimatorSummary3 = ApeEstimatorSummaryIter.clone(
     apeWeight = "entries",
@@ -72,11 +137,11 @@ process.ApeEstimatorSummary3 = ApeEstimatorSummaryIter.clone(
     correctionScaling = 0.6,
     smoothIteration = True,
     smoothFraction = 0.5,
-    InputFile = os.environ['CMSSW_BASE'] + '/src/ApeEstimator/ApeEstimator/hists/test_' + SAMPLE + '.root',
-    ResultsFile = os.environ['CMSSW_BASE'] + '/src/ApeEstimator/ApeEstimator/hists/test_' + SAMPLE + '_resultsFile3.root',
-    BaselineFile = os.environ['CMSSW_BASE'] + '/src/ApeEstimator/ApeEstimator/hists/test_' + SAMPLE + '_baselineApe.root',
-    IterationFile = os.environ['CMSSW_BASE'] + '/src/ApeEstimator/ApeEstimator/hists/test_' + SAMPLE + '_iterationApe3.root',
-    ApeOutputFile = os.environ['CMSSW_BASE'] + '/src/ApeEstimator/ApeEstimator/hists/test_' + SAMPLE + '_apeOutput3.txt',
+    InputFile = os.environ['CMSSW_BASE'] + '/src/ApeEstimator/ApeEstimator/hists/test_' + options.sample + '.root',
+    ResultsFile = os.environ['CMSSW_BASE'] + '/src/ApeEstimator/ApeEstimator/hists/test_' + options.sample + '_resultsFile3.root',
+    BaselineFile = os.environ['CMSSW_BASE'] + '/src/ApeEstimator/ApeEstimator/hists/test_' + options.sample + '_baselineApe.root',
+    IterationFile = os.environ['CMSSW_BASE'] + '/src/ApeEstimator/ApeEstimator/hists/test_' + options.sample + '_iterationApe3.root',
+    ApeOutputFile = os.environ['CMSSW_BASE'] + '/src/ApeEstimator/ApeEstimator/hists/test_' + options.sample + '_apeOutput3.txt',
 )
 
 
