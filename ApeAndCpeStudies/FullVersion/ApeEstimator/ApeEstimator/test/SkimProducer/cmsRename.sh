16b2eb5026a57f1497fb7d77b5eceba4aa085588
hauk <>
2011-10-09 18:59:14 +0000

    Script for renaming files on CAF user pool

---
+++ ApeAndCpeStudies/FullVersion/ApeEstimator/ApeEstimator/test/SkimProducer/cmsRename.sh
@@ -0,0 +1,86 @@
+#!/bin/sh
+
+if [ ! $# == 1 ]; then
+  echo "Usage: $0 sample"
+  exit 1
+fi
+
+
+sample="$1"
+
+directory=""
+if [[ "$sample" == data1 ]] ; then directory="/store/caf/user/hauk/data/DoubleMu/Run2011A_May10ReReco/"
+elif [[ "$sample" == data2 ]] ; then directory="/store/caf/user/hauk/data/DoubleMu/Run2011A_PromptV4/"
+elif [[ "$sample" == qcd ]] ; then directory="/store/caf/user/hauk/mc/Summer11/qcd/"
+elif [[ "$sample" == wlnu ]] ; then directory="/store/caf/user/hauk/mc/Summer11/wlnu/"
+elif [[ "$sample" == zmumu10 ]] ; then directory="/store/caf/user/hauk/mc/Summer11/zmumu10/"
+elif [[ "$sample" == zmumu20 ]] ; then directory="/store/caf/user/hauk/mc/Summer11/zmumu20/"
+else
+  echo "Invalid dataset: $sample"
+  exit 2
+fi
+
+filebase="${directory}apeSkim"
+
+filesuffix=".root"
+
+tempFile="/tmp/hauk/temp.root"
+
+
+## increment counter
+declare -i counter=1
+
+inputFilename="${filebase}${filesuffix}"
+outputFilename="${filebase}${counter}${filesuffix}"
+
+cmsStageIn $inputFilename $tempFile
+cmsStageOut $tempFile $outputFilename
+if [ $? -eq 0 ] ; then
+  cmsRm ${filebase}${filesuffix}
+fi
+rm $tempFile
+
+
+while [ $counter -le 9 ]
+do
+  declare -i counterIncrement=${counter}+1
+  
+  inputFilename="${filebase}00${counter}${filesuffix}"
+  outputFilename="${filebase}${counterIncrement}${filesuffix}"
+  
+  cmsStageIn $inputFilename $tempFile
+  cmsStageOut $tempFile $outputFilename
+  if [ $? -eq 0 ] ; then
+    cmsRm $inputFilename
+  fi
+  rm $tempFile
+  
+  counter=$counter+1
+done
+
+
+### increment counter after first 10 files
+#declare -i counterTen=10
+#
+#while [ $counterTen -le 17 ]
+#do
+#  declare -i counterTenIncrement=${counterTen}+1
+#  
+#  inputFilename="${filebase}0${counterTen}${filesuffix}"
+#  outputFilename="${filebase}${counterTenIncrement}${filesuffix}"
+#  
+#  cmsStageIn $inputFilename $tempFile
+#  cmsStageOut $tempFile $outputFilename
+#  if [ $? -eq 0 ] ; then
+#    cmsRm $inputFilename
+#  fi
+#  rm $tempFile
+#  
+#  counterTen=$counterTen+1
+#done
+
+
+
+
+
+
