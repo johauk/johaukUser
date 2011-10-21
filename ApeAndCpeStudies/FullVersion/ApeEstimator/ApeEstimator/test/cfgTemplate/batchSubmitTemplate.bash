9ffc8cc3c32dcba4ffe3a89412814b0addf8e443
hauk <>
2011-10-21 13:37:33 +0000

    Migration of CMSCAFUSER pool to EOS

--- ApeAndCpeStudies/FullVersion/ApeEstimator/ApeEstimator/test/cfgTemplate/batchSubmitTemplate.bash
+++ ApeAndCpeStudies/FullVersion/ApeEstimator/ApeEstimator/test/cfgTemplate/batchSubmitTemplate.bash
@@ -6,10 +6,7 @@ eval `scram runtime -sh`
 cd -
 
 
-export STAGE_SVCCLASS=cmscafuser
-
-
-rfcp _THE_INPUTBASE__THE_NUMBER_.root reco.root
+cmsStageIn _THE_INPUTBASE__THE_NUMBER_.root reco.root
 
 
 cmsRun $CMSSW_BASE/src/ApeEstimator/ApeEstimator/test/cfgTemplate/apeEstimator_cfg.py_THE_COMMANDS_
