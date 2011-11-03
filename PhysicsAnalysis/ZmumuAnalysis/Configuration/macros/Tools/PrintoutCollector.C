129e64a4611c45ce1ab50065109d911854b13608
hauk <>
2011-11-03 18:57:48 +0000

    New class for collecting printout of different jobs and print in LaTeX formatted table

---
+++ PhysicsAnalysis/ZmumuAnalysis/Configuration/macros/Tools/PrintoutCollector.C
@@ -0,0 +1,66 @@
+#include "PrintoutCollector.h"
+
+#include "ZmumuAnalysis/Configuration/macros/Tools/printLine.h"
+
+
+#include <iostream>
+
+
+
+PrintoutCollector::PrintoutCollector(){}
+
+
+
+PrintoutCollector::~PrintoutCollector(){}
+
+
+
+
+void PrintoutCollector::printTable(){
+  std::vector<std::string>::const_iterator i_print;
+
+  Tools::printLine("Table for nObserved");
+  for(i_print = v_nObservedInOut_.begin(); i_print != v_nObservedInOut_.end(); ++i_print){
+    std::cout<<*i_print;
+  }
+  std::cout<<"\n\n\n";
+
+  Tools::printLine("Table for nSimulation");
+  for(i_print = v_nSimulationInOut_.begin(); i_print != v_nSimulationInOut_.end(); ++i_print){
+    std::cout<<*i_print;
+  }
+  std::cout<<"\n\n\n";
+  
+  Tools::printLine("Table for ratioInOut");
+  for(i_print = v_ratioInOut_.begin(); i_print != v_ratioInOut_.end(); ++i_print){
+    std::cout<<*i_print;
+  }
+  std::cout<<"\n\n\n";
+  
+  Tools::printLine("Table for ttbarFraction");
+  for(i_print = v_ttbarFraction_.begin(); i_print != v_ttbarFraction_.end(); ++i_print){
+    std::cout<<*i_print;
+  }
+  std::cout<<"\n\n\n";
+}
+
+
+
+void PrintoutCollector::hline(){
+  v_nObservedInOut_.push_back("\\hline\n");
+  v_nSimulationInOut_.push_back("\\hline\n");
+  v_ratioInOut_.push_back("\\hline\n");
+  v_ttbarFraction_.push_back("\\hline\n");
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ PhysicsAnalysis/ZmumuAnalysis/Configuration/macros/Tools/PrintoutCollector.h
@@ -0,0 +1,30 @@
+#ifndef PrintoutCollector_h
+#define PrintoutCollector_h
+
+
+#include <vector>
+
+
+class PrintoutCollector{
+  public:
+    
+    PrintoutCollector();
+    ~PrintoutCollector();
+    
+    void printTable();
+    void hline();
+    
+    
+  private:
+    
+    std::vector<std::string> v_nObservedInOut_;
+    std::vector<std::string> v_nSimulationInOut_;
+    std::vector<std::string> v_ratioInOut_;
+    std::vector<std::string> v_ttbarFraction_;
+    
+};
+
+
+#endif
+
+
