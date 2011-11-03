a840b1e0c76cdb0ea811a5848ccd4c203d92abd9
hauk <>
2011-11-03 18:59:08 +0000

    Introduce correct labels for different selection steps and different jobs

--- PhysicsAnalysis/ZmumuAnalysis/Configuration/macros/Drawings/fillTable.h
+++ PhysicsAnalysis/ZmumuAnalysis/Configuration/macros/Drawings/fillTable.h
@@ -9,28 +9,34 @@
 
 
 void FullAnalysis::fillTable(){
-  const std::string selectionStep(this->recoSelectionStep());
+  
+  const std::string selectionStep(this->assignLabel());
+  
   std::stringstream printout;
   std::stringstream parameter1;
   std::stringstream parameter2;
   
-  printout<<std::left<<std::setw(10)<<selectionStep<<" & "
-          <<std::setw(10)<<this->nObserved().value()<<" & "
-	  <<std::setw(10)<<this->nObservedSideband().value()<<" \\\\\n";
-  v_nObservedInOut_.push_back(printout.str());
+  std::stringstream ss_selectionStep;
+  ss_selectionStep<<std::left<<std::setw(16)<<selectionStep<<" & ";
+  
+  printout<<ss_selectionStep.str()
+          <<std::setw(8)<<this->nObserved().value()<<" & "
+	  <<std::setw(8)<<this->nObservedSideband().value()<<" \\\\\n";
+  printoutCollector_.v_nObservedInOut_.push_back(printout.str());
   printout.str("");
   printout.clear();
   
   
-  printout<<std::left<<std::setw(10)<<selectionStep<<" & "
+  printout<<ss_selectionStep.str()
           <<std::fixed<<std::setprecision(1)
 	  <<std::setw(10)<<this->nTtbarInMc()<<" & "
 	  <<std::setw(10)<<this->nTtbarOutMc()<<" & "
 	  <<std::setw(10)<<this->nZmumuInMc()<<" & "
 	  <<std::setw(10)<<this->nZmumuOutMc()<<" & "
+	  <<std::setprecision(0)
 	  <<this->nBackgroundOther().value()<<" & "
 	  <<this->nBackgroundOtherSideband().value()<<" \\\\\n";
-  v_nSimulationInOut_.push_back(printout.str());
+  printoutCollector_.v_nSimulationInOut_.push_back(printout.str());
   printout.str("");
   printout.clear();
   
@@ -39,10 +45,10 @@ void FullAnalysis::fillTable(){
             <<"$"<<this->ratioInOutTtbar().value()<<"\\pm"<<this->ratioInOutTtbar().absErrUp()<<"$";
   parameter2<<std::fixed<<std::setprecision(2)
             <<"$"<<this->ratioInOutZmumu().value()<<"\\pm"<<this->ratioInOutZmumu().absErrUp()<<"$";
-  printout<<std::left<<std::setw(10)<<selectionStep<<" & "
-	  <<std::setw(20)<<parameter1.str()<<" & "
-	  <<std::setw(20)<<parameter2.str()<<" \\\\\n";
-  v_ratioInOut_.push_back(printout.str());
+  printout<<ss_selectionStep.str()
+	  <<std::setw(15)<<parameter1.str()<<" & "
+	  <<std::setw(15)<<parameter2.str()<<" \\\\\n";
+  printoutCollector_.v_ratioInOut_.push_back(printout.str());
   printout.str("");
   printout.clear();
   parameter1.str("");
@@ -55,15 +61,15 @@ void FullAnalysis::fillTable(){
             <<"$"<<this->ttbarEstimate().value()<<"\\pm"<<this->ttbarEstimate().absErrUp()<<"$";
   parameter2<<std::fixed<<std::setprecision(2)
             <<"$"<<100.*this->ttbarFraction().value()<<"\\pm"<<100.*this->ttbarFraction().absErrUp()<<"$";
-  printout<<std::left<<std::setw(10)<<selectionStep<<" & "
+  printout<<ss_selectionStep.str()
 	  <<std::fixed<<std::setprecision(1)
-	  <<std::setw(20)<<parameter1.str()<<" & "
-	  <<std::setw(10)<<this->ttbarEstimateSideband()<<" & "
-	  <<std::setw(20)<<parameter2.str()<<" & "
-	  <<std::setw(10)<<100.*this->ttbarFractionSideband()<<" & "
-	  <<std::setw(10)<<100.*this->zmumuFraction()<<" & "
-	  <<std::setw(10)<<100.*this->zmumuFractionSideband()<<" \\\\\n";
-  v_ttbarFraction_.push_back(printout.str());
+	  <<std::setw(17)<<parameter1.str()<<" & "
+	  <<std::setw(6)<<this->ttbarEstimateSideband()<<" & "
+	  <<std::setw(17)<<parameter2.str()<<" & "
+	  <<std::setw(6)<<100.*this->ttbarFractionSideband()<<" & "
+	  <<std::setw(6)<<100.*this->zmumuFraction()<<" & "
+	  <<std::setw(6)<<100.*this->zmumuFractionSideband()<<" \\\\\n";
+  printoutCollector_.v_ttbarFraction_.push_back(printout.str());
   printout.str("");
   printout.clear();
   parameter1.str("");
@@ -71,6 +77,56 @@ void FullAnalysis::fillTable(){
   parameter2.str("");
   parameter2.clear();
 }
+
+
+
+
+std::string FullAnalysis::assignLabel()const{
+  const std::string& inputFolderName(this->inputFolder());
+  const std::string recoStep(this->recoSelectionStep());
+  if(inputFolderName=="")return recoStep;
+  else if(inputFolderName=="False_HeM__True_default"){
+    if(recoStep=="Step3")return "Dimuons";
+    else if(recoStep=="Step4")return "1 jet";
+    else if(recoStep=="Step5")return "UNDEFINED";
+    else if(recoStep=="Step6")return "1 b (HE)";
+    else if(recoStep=="Step7a")return "UNDEFINED";
+    else if(recoStep=="Step7")return "UNDEFINED";
+    else return "--UNKNOWN STEP--";
+  }
+  else if(inputFolderName=="True_HeM__True_default"){
+    if(recoStep=="Step3")return "UNDEFINED";
+    else if(recoStep=="Step4")return "UNDEFINED";
+    else if(recoStep=="Step5")return "UNDEFINED";
+    else if(recoStep=="Step6")return "1 b (HE) \\& MET";
+    else if(recoStep=="Step7a")return "UNDEFINED";
+    else if(recoStep=="Step7")return "UNDEFINED";
+    else return "--UNKNOWN STEP--";
+  }
+  else if(inputFolderName=="False_HpT_HeM_True_default"){
+    if(recoStep=="Step3")return "UNDEFINED";
+    else if(recoStep=="Step4")return "UNDEFINED";
+    else if(recoStep=="Step5")return "UNDEFINED";
+    else if(recoStep=="Step6")return "1 b (HP)";
+    else if(recoStep=="Step7a")return "UNDEFINED";
+    else if(recoStep=="Step7")return "UNDEFINED";
+    else return "--UNKNOWN STEP--";
+  }
+  else if(inputFolderName=="True_HpT_HeM_True_default"){
+    if(recoStep=="Step3")return "UNDEFINED";
+    else if(recoStep=="Step4")return "UNDEFINED";
+    else if(recoStep=="Step5")return "UNDEFINED";
+    else if(recoStep=="Step6")return "1 b (HP) \\& MET";
+    else if(recoStep=="Step7a")return "+ 2nd jet";
+    else if(recoStep=="Step7")return "+ 2nd b (HE)";
+    else return "--UNKNOWN STEP--";
+  }
+  else return "--UNKNOWN--";
+}
+
+
+
+
 
 
 
