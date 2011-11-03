5356b8ae17ebde5dbab64138bc3a297616a1c596
hauk <>
2011-11-03 15:09:31 +0000

    Adjustments

--- PhysicsAnalysis/ZmumuAnalysis/Configuration/macros/Calculations/ttbarFraction.h
+++ PhysicsAnalysis/ZmumuAnalysis/Configuration/macros/Calculations/ttbarFraction.h
@@ -33,7 +33,7 @@ void FullAnalysis::setTtbarFraction(){
   const double nTtbarOutRel = nTtbarOut/this->nObservedSideband().value();
   const double nZmumuOutRel = nZmumuOut/this->nObservedSideband().value();
   std::cout<<"ttbar in, ttbar out, zmumu in, zmumu out (relative values in %): "
-           <<nTtbarInRel*100<<" , "<<nTtbarOutRel*100<<" , "<<nZmumuInRel*100<<" , "<<nZmumuOutRel*100<<"\n";
+           <<nTtbarInRel*100.<<" , "<<nTtbarOutRel*100.<<" , "<<nZmumuInRel*100.<<" , "<<nZmumuOutRel*100.<<"\n";
   
   // Derivatives for nTtbarIn, needed for error propagation
   const double dNObservedIn = -factor;
@@ -54,38 +54,41 @@ void FullAnalysis::setTtbarFraction(){
   const double absErr2SystContRatioInOutZmumu = std::pow(dRatioInOutZmumu,2)*this->ratioInOutZmumuCorrected().absErr2SystUp();
   
   std::cout<<"\n";
-  std::cout<<"Error contributions to ttbar fraction with central value in %: "<<nTtbarInRel*100<<"\n";
-  std::cout<<"NObservedIn stat. (absolute in %, relative in %): "<<100*std::sqrt(absErr2ContNObservedIn)/this->nObserved().value()<<" , "<<100*std::sqrt(absErr2ContNObservedIn)/nTtbarIn<<"\n";
-  std::cout<<"NOtherIn (absolute in %, relative in %): "<<100*std::sqrt(absErr2ContNOtherIn)/this->nObserved().value()<<" , "<<100*std::sqrt(absErr2ContNOtherIn)/nTtbarIn<<"\n";
-  std::cout<<"NObservedOut stat. (absolute in %, relative in %): "<<100*std::sqrt(absErr2ContNObservedOut)/this->nObserved().value()<<" , "<<100*std::sqrt(absErr2ContNObservedOut)/nTtbarIn<<"\n";
-  std::cout<<"NOtherOut (absolute in %, relative in %): "<<100*std::sqrt(absErr2ContNOtherOut)/this->nObserved().value()<<" , "<<100*std::sqrt(absErr2ContNOtherOut)/nTtbarIn<<"\n";
-  std::cout<<"RatioInOutTtbar stat. (absolute in %, relative in %): "<<100*std::sqrt(absErr2StatContRatioInOutTtbar)/this->nObserved().value()<<" , "<<100*std::sqrt(absErr2StatContRatioInOutTtbar)/nTtbarIn<<"\n";
-  std::cout<<"RatioInOutZmumu stat. (absolute in %, relative in %): "<<100*std::sqrt(absErr2StatContRatioInOutZmumu)/this->nObserved().value()<<" , "<<100*std::sqrt(absErr2StatContRatioInOutZmumu)/nTtbarIn<<"\n";
-  std::cout<<"RatioInOutTtbar syst. (absolute in %, relative in %): "<<100*std::sqrt(absErr2SystContRatioInOutTtbar)/this->nObserved().value()<<" , "<<100*std::sqrt(absErr2SystContRatioInOutTtbar)/nTtbarIn<<"\n";
-  std::cout<<"RatioInOutZmumu syst. (absolute in %, relative in %): "<<100*std::sqrt(absErr2SystContRatioInOutZmumu)/this->nObserved().value()<<" , "<<100*std::sqrt(absErr2SystContRatioInOutZmumu)/nTtbarIn<<"\n";
+  std::cout<<"Error contributions to ttbar fraction with central value in %: "<<nTtbarInRel*100.<<"\n";
+  std::cout<<"NObservedIn stat. (absolute in %, relative in %): "<<100.*std::sqrt(absErr2ContNObservedIn)/this->nObserved().value()<<" , "<<100.*std::sqrt(absErr2ContNObservedIn)/nTtbarIn<<"\n";
+  std::cout<<"NOtherIn (absolute in %, relative in %): "<<100.*std::sqrt(absErr2ContNOtherIn)/this->nObserved().value()<<" , "<<100.*std::sqrt(absErr2ContNOtherIn)/nTtbarIn<<"\n";
+  std::cout<<"NObservedOut stat. (absolute in %, relative in %): "<<100.*std::sqrt(absErr2ContNObservedOut)/this->nObserved().value()<<" , "<<100.*std::sqrt(absErr2ContNObservedOut)/nTtbarIn<<"\n";
+  std::cout<<"NOtherOut (absolute in %, relative in %): "<<100.*std::sqrt(absErr2ContNOtherOut)/this->nObserved().value()<<" , "<<100.*std::sqrt(absErr2ContNOtherOut)/nTtbarIn<<"\n";
+  std::cout<<"RatioInOutTtbar stat. (absolute in %, relative in %): "<<100.*std::sqrt(absErr2StatContRatioInOutTtbar)/this->nObserved().value()<<" , "<<100.*std::sqrt(absErr2StatContRatioInOutTtbar)/nTtbarIn<<"\n";
+  std::cout<<"RatioInOutZmumu stat. (absolute in %, relative in %): "<<100.*std::sqrt(absErr2StatContRatioInOutZmumu)/this->nObserved().value()<<" , "<<100.*std::sqrt(absErr2StatContRatioInOutZmumu)/nTtbarIn<<"\n";
+  std::cout<<"RatioInOutTtbar syst. (absolute in %, relative in %): "<<100.*std::sqrt(absErr2SystContRatioInOutTtbar)/this->nObserved().value()<<" , "<<100.*std::sqrt(absErr2SystContRatioInOutTtbar)/nTtbarIn<<"\n";
+  std::cout<<"RatioInOutZmumu syst. (absolute in %, relative in %): "<<100.*std::sqrt(absErr2SystContRatioInOutZmumu)/this->nObserved().value()<<" , "<<100.*std::sqrt(absErr2SystContRatioInOutZmumu)/nTtbarIn<<"\n";
   
   // Corresponding error for nInTtbar
   // Where to assign error of NOther, stat or syst?
   const double nTtbarInAbsErr2Stat = absErr2ContNObservedIn + absErr2ContNOtherIn + absErr2ContNObservedOut + absErr2ContNOtherOut + absErr2StatContRatioInOutTtbar + absErr2StatContRatioInOutZmumu;
   const double nTtbarInAbsErr2Syst = absErr2SystContRatioInOutTtbar + absErr2SystContRatioInOutZmumu;
   
-  ValueAndErrorStatSyst ttbarEstimate;
-  ttbarEstimate.setValue(nTtbarIn);
-  ttbarEstimate.setAbsErr2StatUp(nTtbarInAbsErr2Stat);
-  ttbarEstimate.setAbsErr2StatDw(nTtbarInAbsErr2Stat);
-  ttbarEstimate.setAbsErr2SystUp(nTtbarInAbsErr2Syst);
-  ttbarEstimate.setAbsErr2SystDw(nTtbarInAbsErr2Syst);
+  ttbarEstimate_.setValue(nTtbarIn);
+  ttbarEstimate_.setAbsErr2StatUp(nTtbarInAbsErr2Stat);
+  ttbarEstimate_.setAbsErr2StatDw(nTtbarInAbsErr2Stat);
+  ttbarEstimate_.setAbsErr2SystUp(nTtbarInAbsErr2Syst);
+  ttbarEstimate_.setAbsErr2SystDw(nTtbarInAbsErr2Syst);
+  ttbarEstimateSideband_ = nTtbarOut;
   
   std::cout<<"\n";
-  std::cout<<"Estimated ttbar contribution (absolute values): "<<ttbarEstimate.print()<<"\n";
+  std::cout<<"Estimated ttbar contribution (absolute values): "<<this->ttbarEstimate().print()<<"\n";
   
   // Final results
   zmumuFraction_ = nZmumuInRel;
-  ttbarFraction_.setValue(ttbarEstimate.value()/this->nObserved().value());
-  ttbarFraction_.setRelErr2StatUp(ttbarEstimate.relErr2StatUp());
-  ttbarFraction_.setRelErr2StatDw(ttbarEstimate.relErr2StatDw());
-  ttbarFraction_.setRelErr2SystUp(ttbarEstimate.relErr2SystUp());
-  ttbarFraction_.setRelErr2SystDw(ttbarEstimate.relErr2SystDw());
+  ttbarFraction_.setValue(this->ttbarEstimate().value()/this->nObserved().value());
+  ttbarFraction_.setRelErr2StatUp(this->ttbarEstimate().relErr2StatUp());
+  ttbarFraction_.setRelErr2StatDw(this->ttbarEstimate().relErr2StatDw());
+  ttbarFraction_.setRelErr2SystUp(this->ttbarEstimate().relErr2SystUp());
+  ttbarFraction_.setRelErr2SystDw(this->ttbarEstimate().relErr2SystDw());
+  zmumuFractionSideband_ = nZmumuOutRel;
+  ttbarFractionSideband_ = nTtbarOutRel;
+  
   std::cout<<"Estimated ttbar contribution (relative values in %): "<<this->ttbarFraction().print(100.)<<"\n";
 }
 
