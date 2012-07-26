a510b7ad232764a600e54ba3b5cf5db7e4c9e6eb
hauk <>
2012-07-26 17:00:26 +0000

    Adjustments

--- ApeAndCpeStudies/FullVersion/ApeEstimator/ApeEstimator/test/cfgTemplateData/createStep1.bash
+++ ApeAndCpeStudies/FullVersion/ApeEstimator/ApeEstimator/test/cfgTemplateData/createStep1.bash
@@ -28,11 +28,11 @@ echo
 
 ## Alignment
 #export alignmentRcd="globalTag"
-#export alignmentRcd="GR10_v6"
+export alignmentRcd="GR10_v6"
 #export alignmentRcd="GR10_v6_plus5"
 #export alignmentRcd="GR10_v6_plus10"
 #export alignmentRcd="GR10_v6_plus15"
-export alignmentRcd="GR10_v6_plus20"
+#export alignmentRcd="GR10_v6_plus20"
 echo "Alignment Record: $alignmentRcd"
 echo
 
